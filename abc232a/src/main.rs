9f1d659588498e0032cb8aaf212ea9c6edcce643
Wataru NAKAI <watnk0806@gmail.com>
2022-02-17 13:09:05 +0900

    ABC232 A; AC!

---
+++ abc232a/src/main.rs
@@ -0,0 +1,11 @@
+use proconio::input;
+use proconio::marker::Chars;
+
+fn main() {
+    input! {
+        s: Chars,
+    }
+    let a = s[0].to_digit(10).unwrap();
+    let b = s[2].to_digit(10).unwrap();
+    println!("{}", a * b);
+}
