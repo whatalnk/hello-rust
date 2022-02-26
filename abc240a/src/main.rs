a8e84f7156bf5480bbcb16b871e5c8c4e60e9ab2
Wataru NAKAI <watnk0806@gmail.com>
2022-02-26 21:41:41 +0900

    ABC240 A; AC!

---
+++ abc240a/src/main.rs
@@ -0,0 +1,15 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        a: i32,
+        b: i32
+    }
+    if (a, b) == (1, 10) || (a, b) == (10, 1) {
+        println!("Yes");
+    } else if (a - b).abs() == 1 {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
