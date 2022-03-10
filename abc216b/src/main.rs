fe8ef0f6abb7d6bafffbb82e296a9c0aebfcdb47
Wataru NAKAI <watnk0806@gmail.com>
2022-03-10 23:31:12 +0900

    ABC216 B; AC!

---
+++ abc216b/src/main.rs
@@ -0,0 +1,20 @@
+use proconio::input;
+use std::collections::HashMap;
+
+fn main() {
+    input! {
+        n: usize,
+        st: [(String, String); n]
+    }
+    let mut h = HashMap::<String, HashMap<String, i32>>::new();
+    for (s, t) in st {
+        let v = h.entry(s).or_insert(HashMap::new());
+        let vv = v.entry(t).or_insert(0);
+        *vv += 1;
+        if *vv == 2 {
+            println!("Yes");
+            return;
+        }
+    }
+    println!("No");
+}
