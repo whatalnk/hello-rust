41f79b732b0edbc3979420de59998689d0105439
Wataru NAKAI <watnk0806@gmail.com>
2022-03-15 19:47:44 +0900

    Editorial ver.

--- abc218b/src/main.rs
+++ abc218b/src/main.rs
@@ -2,9 +2,9 @@ use proconio::input;
 
 fn main() {
     input! {
-        p: [usize; 26]
+        p: [u8; 26]
     }
-    let a: Vec<&str> = "abcdefghijklmnopqrstuvwxyz".split("").collect();
-    let s: String = p.iter().map(|i| a[*i]).collect();
+    let a = 'a' as u8;
+    let s: String = p.iter().map(|i| (a + i - 1) as char).collect();
     println!("{}", s);
 }
