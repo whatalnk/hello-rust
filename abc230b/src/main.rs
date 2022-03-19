844e3eb3345f5dc4fcc862a89b844311f5fb5219
Wataru NAKAI <watnk0806@gmail.com>
2022-03-19 21:42:14 +0900

    ABC230 B; AC!

---
+++ abc230b/src/main.rs
@@ -0,0 +1,18 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        s: String
+    }
+    let mut t = "oxx".to_string();
+    for _ in 0..20 {
+        t += "oxx";
+    }
+    let tt = t.as_str();
+    let n = s.len();
+    if s == &tt[0..n] || s == &tt[1..n + 1] || s == &tt[2..n + 2] {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
