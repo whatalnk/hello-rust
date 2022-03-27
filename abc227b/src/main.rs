58738e212d2364c2b7f5e1a410accca3cba07c10
Wataru NAKAI <watnk0806@gmail.com>
2022-03-28 00:53:08 +0900

    ABC227 B; AC!

---
+++ abc227b/src/main.rs
@@ -0,0 +1,22 @@
+use proconio::input;
+
+fn main() {
+    input! {
+       n: usize,
+       s: [usize; n]
+    }
+    let mut v = vec![0; 10000000];
+    for i in 1..=250 {
+        for j in 1..=250 {
+            let ss = 4 * i * j + 3 * i + 3 * j;
+            v[ss] += 1;
+        }
+    }
+    let mut ans = 0;
+    for i in s {
+        if v[i] == 0 {
+            ans += 1;
+        }
+    }
+    println!("{}", ans);
+}
