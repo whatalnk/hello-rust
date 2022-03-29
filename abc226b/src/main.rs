140d2cfbc584dc18452d16bfb977d1f47bf2daea
Wataru NAKAI <watnk0806@gmail.com>
2022-03-29 23:49:29 +0900

    ABC226 B; AC!

--- abc226b/src/main.rs
+++ abc226b/src/main.rs
@@ -1,45 +1,17 @@
 use proconio::input;
-use std::collections::HashMap;
+use std::collections::HashSet;
 
 fn main() {
     input! {
        n: usize
     }
-    let mut hm = HashMap::new();
-    let mut g = Vec::<Vec<i32>>::new();
-    for i in 0..n {
+    let mut hs = HashSet::new();
+    for _ in 0..n {
         input! {
             l: usize,
             a: [i32; l]
         }
-        g.push(a);
-        let v = hm.entry(l).or_insert(Vec::<usize>::new());
-        v.push(i);
+        hs.insert(a);
     }
-    let mut checked = vec![false; n];
-    let mut ans = 0;
-    for (_, v) in &hm {
-        if v.len() == 1 {
-            ans += 1;
-            continue;
-        }
-        for i in 0..v.len() {
-            if checked[i] {
-                continue;
-            } else {
-                checked[i] = true;
-                ans += 1;
-            }
-            for j in (i + 1)..v.len() {
-                if checked[j] {
-                    continue;
-                } else {
-                    if g[v[i]] == g[v[j]] {
-                        checked[j] = true;
-                    }
-                }
-            }
-        }
-    }
-    println!("{}", ans);
+    println!("{}", hs.len());
 }
