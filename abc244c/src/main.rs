8afc4a4b75976996c040d6b14f37eb92c863ec0b
Wataru NAKAI <watnk0806@gmail.com>
2022-03-31 19:54:53 +0900

    ABC244 C; fix input to handle interactive problem

--- abc244c/src/main.rs
+++ abc244c/src/main.rs
@@ -1,33 +1,30 @@
 use proconio::input;
-use std::collections::VecDeque;
+use proconio::source::line::LineSource;
+use std::io::{stdin, BufReader};
 
 fn main() {
+    let stdin = stdin();
+    let mut source = LineSource::new(BufReader::new(stdin.lock()));
     input! {
+        from &mut source,
         n: usize
     }
     let mut checked = vec![false; 2 * n + 2];
-    let mut deq: VecDeque<usize> = VecDeque::new();
-    for i in 1..=(2 * n + 1) {
-        deq.push_back(i);
-    }
-    let a = deq.pop_front().unwrap();
-    checked[a] = true;
-    println!("{}", a);
     loop {
-        input! {
-            k: usize
-        }
-        if k == 0 {
-            return;
-        }
-        checked[k] = true;
-        loop {
-            let a = deq.pop_front().unwrap();
-            if !checked[a] {
-                println!("{}", a);
-                checked[a] = true;
+        for i in 1..=(2 * n + 1) {
+            if !checked[i] {
+                println!("{}", i);
+                checked[i] = true;
                 break;
             }
         }
+        input! {
+            from &mut source,
+            k: usize
+        }
+        if k == 0 {
+            break;
+        }
+        checked[k] = true;
     }
 }
