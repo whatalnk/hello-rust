2f551353946cdcd6bcfb7072e47712cd3c836a8d
Wataru NAKAI <watnk0806@gmail.com>
2022-04-08 11:16:06 +0900

    ABC239 C; AC!

---
+++ abc239c/src/main.rs
@@ -0,0 +1,25 @@
+use proconio::input;
+use std::collections::HashSet;
+
+fn main() {
+    input! {
+        x1: i64,
+        y1: i64,
+        x2: i64,
+        y2: i64
+    }
+    let dx = [1, 2, 2, 1, -1, -2, -2, -1];
+    let dy = [2, 1, -1, -2, -2, -1, 1, 2];
+    let mut ps1 = HashSet::new();
+    let mut ps2 = HashSet::new();
+    for i in 0..8 {
+        ps1.insert((x1 + dx[i], y1 + dy[i]));
+        ps2.insert((x2 + dx[i], y2 + dy[i]));
+    }
+    let intersection: HashSet<_> = ps1.intersection(&ps2).collect();
+    if intersection.len() > 0 {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
