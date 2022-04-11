12f9b1f38cd3159cbbf3eb5534489ecae32f158f
Wataru NAKAI <watnk0806@gmail.com>
2022-04-11 21:58:51 +0900

    ABC238 C; cont.

--- abc238c/src/main.rs
+++ abc238c/src/main.rs
@@ -1,8 +1,18 @@
 use proconio::input;
 
+fn f(mut x: i64) -> i64 {
+    let mut ans = 0;
+    while x > 0 {
+        x /= 10;
+        ans += 1;
+    }
+    return ans;
+}
+
 fn main() {
     input! {
         n: i64
     }
-    println!("{}", n);
+    let ans = f(n);
+    println!("{} {}", n, ans);
 }
