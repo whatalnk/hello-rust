2bc4e9a2e01d8fec07bd975d8c5acbecc33e4d73
Wataru NAKAI <watnk0806@gmail.com>
2022-04-12 11:07:26 +0900

    ABC237 C; AC!

--- abc237c/src/main.rs
+++ abc237c/src/main.rs
@@ -13,16 +13,27 @@ fn main() {
     }
     let mut ans = false;
     let n = s.len();
+    let mut left = 0;
     for i in 0..n {
-        if s[n - 1 - i] == 'a' {
-            let a: String = s[0..(n - 1 - i)].iter().collect();
-            let b: String = s[0..(n - 1 - i)].iter().rev().collect();
-            if a == b {
-                ans = true;
-                break;
-            }
+        if s[i] == 'a' {
+            left += 1;
         } else {
             break;
+        }
+    }
+    let mut right = 0;
+    for i in 0..n {
+        if s[n - 1 - i] == 'a' {
+            right += 1;
+        } else {
+            break;
+        }
+    }
+    if left <= right {
+        let a: String = s[left..(n - right)].iter().collect();
+        let b: String = s[left..(n - right)].iter().rev().collect();
+        if a == b {
+            ans = true;
         }
     }
     if ans {
