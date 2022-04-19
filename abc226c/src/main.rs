34345c15c9beed3e1ca516a384c42f54c06eb599
Wataru NAKAI <watnk0806@gmail.com>
2022-04-20 00:59:13 +0900

    ABC226 C; TLE

---
+++ abc226c/src/main.rs
@@ -0,0 +1,37 @@
+use proconio::input;
+use std::collections::HashSet;
+
+fn main() {
+    input! {
+        n: usize
+    }
+    let mut tt = vec![];
+    let mut aa = vec![];
+    for _ in 0..n {
+        input! {
+            t: i128,
+            k: usize,
+            a: [usize; k]
+        }
+        tt.push(t);
+        aa.push(a);
+    }
+    let mut hs = HashSet::new();
+    let mut q = vec![];
+    hs.insert(n);
+    q.push(n);
+    while q.len() > 0 {
+        let i = q.pop().unwrap();
+        for a in &aa[i - 1] {
+            hs.insert(*a);
+            if a != &0 {
+                q.push(*a);
+            }
+        }
+    }
+    let mut ans: i128 = 0;
+    for i in &hs {
+        ans += tt[*i - 1];
+    }
+    println!("{}", ans);
+}
