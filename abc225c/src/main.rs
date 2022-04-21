04e0d42fa57d155bd97a4a5779be126f2fe0363b
Wataru NAKAI <watnk0806@gmail.com>
2022-04-21 12:20:47 +0900

    ABC225 C; AC...

--- abc225c/src/main.rs
+++ abc225c/src/main.rs
@@ -6,20 +6,29 @@ fn main() {
         m: usize,
         b: [[i64; m]; n]
     }
-    let mut ans = true;
-    for i in 0..(n - 1) {
+    let mut x = vec![vec![0; m]; n];
+    let mut y = vec![vec![0; m]; n];
+
+    for i in 0..n {
         for j in 0..m {
-            if b[i + 1][j] - b[i][j] != 7 {
-                ans = false;
-                break;
-            }
+            x[i][j] = (b[i][j] + 6) / 7;
+            y[i][j] = (b[i][j] - 1) % 7 + 1;
         }
     }
+    let mut ans = true;
     for i in 0..n {
-        for j in 0..(m - 1) {
-            if b[i][j + 1] - b[i][j] != 1 {
+        for j in 0..m {
+            if i > 0 && x[i][j] != x[i - 1][j] + 1 {
                 ans = false;
-                break;
+            }
+            if j > 0 && y[i][j] != y[i][j - 1] + 1 {
+                ans = false;
+            }
+            if j > 0 && x[i][j] != x[i][j - 1] {
+                ans = false;
+            }
+            if i > 0 && y[i][j] != y[i - 1][j] {
+                ans = false;
             }
         }
     }
