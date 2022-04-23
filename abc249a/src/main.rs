bfe38d3c03348300f700f921c4285fdcb6edeea8
Wataru NAKAI <watnk0806@gmail.com>
2022-04-23 22:47:52 +0900

    ABC249 A; AC!

---
+++ abc249a/src/main.rs
@@ -0,0 +1,62 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        a: usize,
+        b: i32,
+        c: usize,
+        d: usize,
+        e: i32,
+        f: usize,
+        x: usize
+    }
+    let mut tak = vec![0; x + 1];
+    let mut aok = vec![0; x + 1];
+    let mut cnt = 0;
+    'outer1: loop {
+        for _ in 0..a {
+            if cnt > x {
+                break 'outer1;
+            }
+            tak[cnt] = b;
+            cnt += 1;
+        }
+        for _ in 0..c {
+            if cnt > x {
+                break 'outer1;
+            }
+            tak[cnt] = 0;
+            cnt += 1;
+        }
+    }
+    let mut cnt = 0;
+    'outer2: loop {
+        for _ in 0..d {
+            if cnt > x {
+                break 'outer2;
+            }
+            aok[cnt] = e;
+            cnt += 1;
+        }
+        for _ in 0..f {
+            if cnt > x {
+                break 'outer2;
+            }
+            aok[cnt] = 0;
+            cnt += 1;
+        }
+    }
+    let mut ans1 = 0;
+    let mut ans2 = 0;
+    for i in 0..x {
+        ans1 += tak[i];
+        ans2 += aok[i];
+    }
+    if ans1 > ans2 {
+        println!("Takahashi");
+    } else if ans1 == ans2 {
+        println!("Draw");
+    } else {
+        println!("Aoki");
+    }
+}
