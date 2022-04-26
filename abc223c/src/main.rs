24928e24a4dd24fa69500a0df81255741e77c6f9
Wataru NAKAI <watnk0806@gmail.com>
2022-04-26 22:03:07 +0900

    ABC223 C; AC...

--- abc223c/src/main.rs
+++ abc223c/src/main.rs
@@ -3,10 +3,17 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        ab: [(i32, i32); n]
+        ab: [(f64, f64); n]
     }
-    println!("{}", n);
+    let mut t = 0.0;
+    let mut ans = 0.0;
     for i in 0..n {
-        println!("{} {}", ab[i].0, ab[i].1);
+        t += ab[i].0 / ab[i].1;
     }
+    t /= 2.0;
+    for i in 0..n {
+        ans += (ab[i].0).min(t * ab[i].1);
+        t -= (ab[i].0 / ab[i].1).min(t);
+    }
+    println!("{}", ans);
 }
