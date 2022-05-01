6db05344cd70666f9d68351a6315cc30507b02a3
Wataru NAKAI <watnk0806@gmail.com>
2022-05-02 01:41:51 +0900

    ABC206 C; AC...

--- abc206c/src/main.rs
+++ abc206c/src/main.rs
@@ -1,15 +1,19 @@
 use proconio::input;
+use std::collections::HashMap;
 
 fn main() {
     input! {
         n: usize,
         a: [i64; n]
     }
-    let mut ans = 0;
-    for i in 0..n {
-        let c = a[(i + 1)..].iter().filter(|&x| *x == a[i]).count();
-        ans += n - i - 1;
-        ans -= c;
+    let mut hm = HashMap::<&i64, i64>::new();
+    for x in &a {
+        let e = hm.entry(x).or_insert(0);
+        *e += 1;
+    }
+    let mut ans = (n * (n - 1) / 2) as i64;
+    for (_, v) in &hm {
+        ans -= v * (v - 1) / 2;
     }
     println!("{}", ans);
 }
