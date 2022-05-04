780fd2d8ad440831867b03f9928dda0cf117f19f
Wataru NAKAI <watnk0806@gmail.com>
2022-05-04 19:48:36 +0900

    ABC200 C; TLE

--- abc200c/src/main.rs
+++ abc200c/src/main.rs
@@ -1,10 +1,25 @@
 use proconio::input;
+use std::collections::HashMap;
 
 fn main() {
     input! {
         n: usize,
         a: [i64; n],
     }
-    println!("{}", n);
-    println!("{:?}", a);
+    let mut hm = HashMap::new();
+    for i in 0..n {
+        let e = hm.entry(a[i] % 200).or_insert(vec![]);
+        e.push(i);
+    }
+    let mut ans = 0;
+    for (_, v) in &hm {
+        for i in 0..(v.len()) {
+            for j in (i + 1)..(v.len()) {
+                if (a[v[i]] - a[v[j]]) % 200 == 0 {
+                    ans += 1;
+                }
+            }
+        }
+    }
+    println!("{}", ans);
 }
