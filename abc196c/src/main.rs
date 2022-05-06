7208da950012f47924ae08142ebaad81518c8fcb
Wataru NAKAI <watnk0806@gmail.com>
2022-05-06 19:16:50 +0900

    ABC196 C; AC!

---
+++ abc196c/src/main.rs
@@ -0,0 +1,25 @@
+use proconio::input;
+
+fn f(mut n: i128) -> u32 {
+    let mut ans = 0;
+    while n > 0 {
+        ans += 1;
+        n /= 10;
+    }
+    return ans;
+}
+
+fn main() {
+    input! {
+        n: i128,
+    }
+    let l = f(n) / 2;
+    let mut ans = 0;
+    for i in 1..((10i128).pow(l)) {
+        if i + i * 10i128.pow(f(i)) > n {
+            break;
+        }
+        ans += 1;
+    }
+    println!("{}", ans);
+}
