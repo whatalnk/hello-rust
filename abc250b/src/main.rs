06acb2e77b3157ececc4cd0937f040cf381721f6
Wataru NAKAI <watnk0806@gmail.com>
2022-05-09 16:23:32 +0900

    ABC250 B; refactor

--- abc250b/src/main.rs
+++ abc250b/src/main.rs
@@ -6,15 +6,17 @@ fn main() {
         a: usize,
         b: usize
     }
-    let mut r1 = Vec::new();
-    let mut r2 = Vec::new();
+    let mut r1 = vec![""];
+    let mut r2 = vec![""];
+    let white = ".".repeat(b);
+    let black = "#".repeat(b);
     for i in 0..n {
         if (i + 1) % 2 != 0 {
-            r1.push(".".repeat(b));
-            r2.push("#".repeat(b));
+            r1.push(&white);
+            r2.push(&black);
         } else {
-            r1.push("#".repeat(b));
-            r2.push(".".repeat(b));
+            r1.push(&black);
+            r2.push(&white);
         }
     }
     let r1s: String = vec![r1.join(""); a].join("\n");
