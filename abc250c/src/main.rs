083365352c64b3f9f873bdd828ce96334dd4e7a3
Wataru NAKAI <watnk0806@gmail.com>
2022-05-11 22:49:50 +0900

    ABC250 C; AC!

--- abc250c/src/main.rs
+++ abc250c/src/main.rs
@@ -1,11 +1,35 @@
 use proconio::input;
+use std::collections::HashMap;
 
 fn main() {
     input! {
         n: usize,
         q: usize,
-        x: [usize; n]
+        x: [usize; q]
     }
-    println!("{} {}", n, q);
-    println!("{:?}", x);
+    let mut b = vec![];
+    for i in 1..=n {
+        b.push(i);
+    }
+    let mut hm = HashMap::new();
+    for i in 1..=n {
+        hm.insert(i, i);
+    }
+    for i in x {
+        let l = hm.get(&i).unwrap();
+        let mut r = l + 1;
+        if l == &n {
+            r = l - 1;
+        }
+        b.swap(l - 1, r - 1);
+        hm.insert(b[l - 1], *l);
+        hm.insert(b[r - 1], r);
+    }
+    println!(
+        "{}",
+        b.iter()
+            .map(|ii| ii.to_string())
+            .collect::<Vec<String>>()
+            .join(" ")
+    );
 }
