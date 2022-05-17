83d415db9437604dd6f36d606997c95fe9921afe
Wataru NAKAI <watnk0806@gmail.com>
2022-05-17 21:50:30 +0900

    ABC209 C; AC...

--- abc209c/src/main.rs
+++ abc209c/src/main.rs
@@ -3,12 +3,12 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        c: [i128; n]
+        mut c: [i128; n]
     }
+    c.sort();
     let m: i128 = 1_000_000_000 + 7;
-    let mut ans: i128 = c[0];
-    ans %= m;
-    for i in 1..n {
+    let mut ans: i128 = 1;
+    for i in 0..n {
         ans *= c[i] - i as i128;
         ans %= m;
     }
