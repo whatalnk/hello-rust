7b331514414587b48319f6bf2e3d31f7fb6781c6
Wataru NAKAI <watnk0806@gmail.com>
2022-05-26 00:25:59 +0900

    ABC204 C; AC!

---
+++ abc204c/src/main.rs
@@ -0,0 +1,33 @@
+use proconio::input;
+use std::collections::VecDeque;
+
+fn main() {
+    input! {
+        n: usize,
+        m: usize,
+        ab: [(usize, usize); m]
+    }
+    let mut g = vec![vec![]; n];
+    for i in 0..m {
+        g[ab[i].0 - 1].push(ab[i].1 - 1);
+    }
+    let mut ans = 0;
+    for i in 0..n {
+        let mut visited = vec![0; n];
+        visited[i] = 1;
+        ans += 1;
+        let mut que = VecDeque::new();
+        que.push_back(i);
+        while que.len() > 0 {
+            let cur = que.pop_front().unwrap();
+            for j in &g[cur] {
+                if visited[*j] == 0 {
+                    visited[*j] = 1;
+                    ans += 1;
+                    que.push_back(*j);
+                }
+            }
+        }
+    }
+    println!("{}", ans);
+}
