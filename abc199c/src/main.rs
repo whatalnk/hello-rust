a9f058dc68d3c58514690353cba9e2b5cc34c555
Wataru NAKAI <watnk0806@gmail.com>
2022-05-27 22:27:11 +0900

    ABC199 C; AC!

---
+++ abc199c/src/main.rs
@@ -0,0 +1,48 @@
+use proconio::input;
+use proconio::marker::Chars;
+
+fn main() {
+    input! {
+        n: usize,
+        s: Chars,
+        q: usize,
+        tab: [(i32, usize, usize); q]
+    }
+    let mut v: Vec<usize> = (0..(2 * n)).collect();
+    let mut flipped = false;
+    for i in 0..q {
+        let (t, a, b) = tab[i];
+        if t == 1 {
+            if flipped {
+                let a_: usize;
+                let b_: usize;
+                if a > n {
+                    a_ = a - n;
+                } else {
+                    a_ = a + n;
+                }
+                if b > n {
+                    b_ = b - n;
+                } else {
+                    b_ = b + n;
+                }
+                v.swap(a_ - 1, b_ - 1);
+            } else {
+                v.swap(a - 1, b - 1);
+            }
+        } else {
+            if flipped {
+                flipped = false;
+            } else {
+                flipped = true;
+            }
+        }
+    }
+    let left: String = v[0..n].iter().map(|i| s[*i]).collect();
+    let right: String = v[n..(2 * n)].iter().map(|i| s[*i]).collect();
+    if flipped {
+        println!("{}{}", right, left);
+    } else {
+        println!("{}{}", left, right);
+    }
+}
