ed2c1fa3edc2affe7ad9490c53ad7b3d18ace9da
Wataru NAKAI <watnk0806@gmail.com>
2022-05-28 23:00:19 +0900

    ABC253 C; AC...

--- abc253c/src/main.rs
+++ abc253c/src/main.rs
@@ -1,10 +1,12 @@
 use proconio::input;
+use std::collections::BTreeSet;
 use std::collections::HashMap;
 
 fn main() {
     input!(q: usize);
 
     let mut s = HashMap::new();
+    let mut bs = BTreeSet::new();
 
     for _ in 0..q {
         input!(kind: u32);
@@ -13,20 +15,22 @@ fn main() {
                 input!(x: i32);
                 let e = s.entry(x).or_insert(0);
                 *e += 1;
+                bs.insert(x);
             }
             2 => {
                 input!(x: i32, c: i32);
                 if let Some(v) = s.get(&x) {
                     if &c >= v {
                         s.remove(&x);
+                        bs.remove(&x);
                     } else {
                         s.entry(x).and_modify(|e| *e -= c);
                     }
                 }
             }
             3 => {
-                let xmin = s.keys().min().unwrap();
-                let xmax = s.keys().max().unwrap();
+                let xmin = bs.iter().nth(0).unwrap();
+                let xmax = bs.iter().last().unwrap();
                 println!("{}", xmax - xmin);
             }
             _ => unreachable!(),
