b9e7ac188a78970ad2177b96371c5fdf967767a5
Wataru NAKAI <watnk0806@gmail.com>
2022-05-28 16:53:07 +0900

    ABC178 C; AC...

---
+++ abc178c/src/main.rs
@@ -0,0 +1,20 @@
+use proconio::input;
+
+const MD: i64 = 1000000007;
+fn powmod(x: i64, y: usize) -> i64 {
+    let mut res = 1;
+    for _ in 0..y {
+        res = res * x % MD;
+    }
+    return res;
+}
+
+fn main() {
+    input! {
+        n: usize
+    }
+    let mut ans = powmod(10, n) - powmod(9, n) - powmod(9, n) + powmod(8, n);
+    ans %= MD;
+    ans = (ans + MD) % MD;
+    println!("{}", ans);
+}
