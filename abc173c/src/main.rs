3945e1b8daa06c18e2caaff3ca9e9c60ca259e0e
Wataru NAKAI <watnk0806@gmail.com>
2022-05-29 22:21:26 +0900

    ABC173 C; AC!

---
+++ abc173c/src/main.rs
@@ -0,0 +1,52 @@
+use itertools::Itertools;
+use proconio::input;
+use proconio::marker::Chars;
+
+fn main() {
+    input! {
+        h: usize,
+        w: usize,
+        k: usize,
+        c: [Chars; h]
+    }
+    let mut hh = vec![];
+    let mut ww = vec![];
+    for n in 0..=h {
+        for i in (0..h).combinations(n) {
+            hh.push(i);
+        }
+    }
+    for n in 0..=w {
+        for i in (0..w).combinations(n) {
+            ww.push(i);
+        }
+    }
+    let mut ans = 0;
+    for i in &hh {
+        for j in &ww {
+            let mut m = vec![vec![true; w]; h];
+            for ii in i {
+                for jj in 0..w {
+                    m[ii.clone()][jj] = false;
+                }
+            }
+            for jj in j {
+                for ii in 0..h {
+                    m[ii][jj.clone()] = false;
+                }
+            }
+            let mut cnt = 0;
+            for ii in 0..h {
+                for jj in 0..w {
+                    if m[ii][jj] && c[ii][jj] == '#' {
+                        cnt += 1;
+                    }
+                }
+            }
+            if cnt == k {
+                ans += 1;
+            }
+        }
+    }
+    println!("{}", ans);
+}
