865cee27f6e2c2aed32f1fab855e734eabb21537
Wataru NAKAI <watnk0806@gmail.com>
2022-06-02 22:11:07 +0900

    ABC172 C; AC!

--- abc172c/src/main.rs
+++ abc172c/src/main.rs
@@ -8,24 +8,22 @@ fn main() {
         a: [i64; n],
         b: [i64; m]
     }
-    let mut aa = vec![0; n];
-    let mut bb = vec![0; m];
-    aa[0] = a[0];
-    bb[0] = b[0];
-    for i in 1..n {
-        aa[i] = aa[i - 1] + a[i];
+    let mut aa = vec![0; n + 1];
+    let mut bb = vec![0; m + 1];
+    for i in 0..n {
+        aa[i + 1] = aa[i] + a[i];
     }
-    for i in 1..m {
-        bb[i] = bb[i - 1] + b[i];
+    for i in 0..m {
+        bb[i + 1] = bb[i] + b[i];
     }
     let mut ans = 0;
-    for i in 0..n {
-        if aa[i] < k {
+    for i in 0..(n + 1) {
+        if aa[i] <= k {
             let v = match bb.binary_search(&(k - aa[i])) {
-                Ok(x) => x + 1,
-                Err(x) => x,
+                Ok(x) => x,
+                Err(x) => x - 1,
             };
-            ans = ans.max(i + 1 + v);
+            ans = ans.max(i + v);
         } else {
             break;
         }
