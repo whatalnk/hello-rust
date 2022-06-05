036569f2d59d85b714211242a13d3ad2147029a8
Wataru NAKAI <watnk0806@gmail.com>
2022-06-05 22:53:50 +0900

    ABC218 C; AC...

--- abc218c/src/main.rs
+++ abc218c/src/main.rs
@@ -1,7 +1,7 @@
 use proconio::input;
 use proconio::marker::Chars;
 
-fn rot(s: Vec<Vec<char>>) -> Vec<Vec<char>> {
+fn rot(s: &Vec<Vec<char>>) -> Vec<Vec<char>> {
     let mut ss = vec![];
     let n = s.len();
     for j in 0..n {
@@ -14,7 +14,7 @@ fn rot(s: Vec<Vec<char>>) -> Vec<Vec<char>> {
     return ss;
 }
 
-fn find_left_top(s: Vec<Vec<char>>) -> (usize, usize) {
+fn find_left_top(s: &Vec<Vec<char>>) -> (usize, usize) {
     let n = s.len();
     let mut ans = (0, 0);
     for i in 0..n {
@@ -28,23 +28,23 @@ fn find_left_top(s: Vec<Vec<char>>) -> (usize, usize) {
     return ans;
 }
 
-fn is_same(s: Vec<Vec<char>>, t: Vec<Vec<char>>) -> bool {
+fn is_same(s: &Vec<Vec<char>>, t: &Vec<Vec<char>>) -> bool {
     let n = s.len();
-    let (si, sj) = find_left_top(s.clone());
-    let (ti, tj) = find_left_top(t.clone());
-    let offset_i = ti - si;
-    let offset_j = tj - sj;
+    let (si, sj) = find_left_top(&s);
+    let (ti, tj) = find_left_top(&t);
+    let offset_i = ti as i64 - si as i64;
+    let offset_j = tj as i64 - sj as i64;
     for i in 0..n {
         for j in 0..n {
-            let ii = i + offset_i;
-            let jj = j + offset_j;
-            if ii >= 0 && ii < n && jj >= 0 && jj < n {
-                if s[i][j] != t[ii][jj] {
+            let ii = i as i64 + offset_i;
+            let jj = j as i64 + offset_j;
+            if ii >= 0 && ii < n as i64 && jj >= 0 && jj < n as i64 {
+                if s[i][j] != t[ii as usize][jj as usize] {
                     return false;
-                } else {
-                    if s[i][j] == '#' {
-                        return false;
-                    }
+                }
+            } else {
+                if s[i][j] == '#' {
+                    return false;
                 }
             }
         }
@@ -58,29 +58,19 @@ fn main() {
         s: [Chars; n],
         t : [Chars; n]
     }
-    let mut cnt_s = 0;
-    let mut cnt_t = 0;
-    for i in 0..n {
-        for j in 0..n {
-            if s[i][j] == '#' {
-                cnt_s += 1;
-            }
-            if t[i][j] == '#' {
-                cnt_t += 1;
-            }
-        }
-    }
+    let cnt_s = s.iter().flatten().filter(|x| x == &&'#').count();
+    let cnt_t = t.iter().flatten().filter(|x| x == &&'#').count();
     if cnt_s != cnt_t {
         println!("No");
         return;
     }
     let mut s_ = s.clone();
     for _ in 0..4 {
-        if is_same(s_.clone(), t.clone()) {
+        if is_same(&s_, &t) {
             println!("Yes");
             return;
         }
-        s_ = rot(s_);
+        s_ = rot(&s_);
     }
     println!("No");
     return;
