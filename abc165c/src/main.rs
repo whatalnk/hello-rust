ca9dba4694847bc9e198ee6d08df457ca33b0ca9
Wataru NAKAI <watnk0806@gmail.com>
2022-06-06 23:41:13 +0900

    ABC165 C; AC!

--- abc165c/src/main.rs
+++ abc165c/src/main.rs
@@ -1,14 +1,52 @@
 use proconio::input;
+use std::cell::RefCell;
+
+struct S {
+    n: usize,
+    m: i64,
+    aa: RefCell<Vec<Vec<i64>>>,
+}
+
+impl S {
+    fn dfs(&self, a: i64, v: Vec<i64>) {
+        if v.len() == self.n {
+            self.aa.borrow_mut().push(v.clone());
+            return;
+        }
+        for i in a..=self.m {
+            let mut vv = v.clone();
+            vv.push(i);
+            self.dfs(i, vv);
+        }
+    }
+}
 
 fn main() {
     input! {
         n: usize,
         m: i64,
         q: usize,
-        abcd: [(i64, i64, i64, i64); q]
+        abcd: [(usize, usize, i64, i64); q]
     }
-    println!("{} {} {}", n, m, q);
-    for i in 0..q {
-        println!("{:?}", abcd[i]);
+    let aa = Vec::<Vec<i64>>::new();
+    let s = S {
+        n: n,
+        m: m,
+        aa: RefCell::new(aa),
+    };
+    s.dfs(1, vec![]);
+    let n = s.aa.borrow().len();
+    let mut ans = 0;
+    for i in 0..n {
+        let ary = &s.aa.borrow()[i];
+        let mut ans_ = 0;
+        for j in 0..q {
+            let (a, b, c, d) = abcd[j];
+            if ary[b - 1] - ary[a - 1] == c {
+                ans_ += d;
+            }
+        }
+        ans = ans.max(ans_);
     }
+    println!("{}", ans);
 }
