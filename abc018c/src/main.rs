0afbabc87ad1c4baa1e81896e9630617b5491d11
Wataru NAKAI <watnk0806@gmail.com>
2022-06-07 22:06:36 +0900

    ABC018 C; cont.

---
+++ abc018c/src/main.rs
@@ -0,0 +1,14 @@
+use proconio::input;
+use proconio::marker::Chars;
+
+fn main() {
+    input! {
+        t: usize,
+        c: usize,
+        k: usize,
+        s: [Chars; c]
+    }
+    let mut ans = 0;
+
+    println!("{}", ans);
+}
