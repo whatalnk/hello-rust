b714be3980ac8a9d6229a7f3709dd6cfa287afbe
Wataru NAKAI <watnk0806@gmail.com>
2022-06-11 23:24:07 +0900

    ABC 253 D; AC...

--- abc253d/src/main.rs
+++ abc253d/src/main.rs
@@ -1,17 +1,19 @@
+use num::integer::lcm;
 use proconio::input;
 
 fn main() {
     input! {
-        n: i64,
-        a: i64,
-        b: i64
+        n: i128,
+        a: i128,
+        b: i128
     }
+    let ab = lcm(a, b);
     let na = n / a;
     let nb = n / b;
-    let nab = n / (a * b);
+    let nab = n / ab;
     let s = (1 + n) * n / 2;
     let sa = a * (1 + na) * na / 2;
     let sb = b * (1 + nb) * nb / 2;
-    let sab = a * b * (1 + nab) * nab / 2;
+    let sab = ab * (1 + nab) * nab / 2;
     println!("{}", s - (sa + sb - sab));
 }
