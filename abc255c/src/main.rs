a51a1294f7d428ba6a51c59907bd19214cd31686
Wataru NAKAI <watnk0806@gmail.com>
2022-06-14 23:59:36 +0900

    ABC255 C; AC...

--- abc255c/src/main.rs
+++ abc255c/src/main.rs
@@ -3,29 +3,26 @@ use proconio::input;
 fn main() {
     input! {
         x: i128,
-        a: i128,
-        d: i128,
+        mut a: i128,
+        mut d: i128,
         n: i128
     }
-    let mut ans = 0;
-    let s0 = a;
-    let sn = a + (n - 1) * d;
-    if d > 0 {
-        if x < s0 {
-            ans = s0 - x;
-        } else if x < sn {
-            ans = (x - a) % d;
-        } else {
-            ans = x - sn;
-        }
-    } else {
-        if x < sn {
-            ans = sn - x;
-        } else if x < sn {
-            ans = (x - a) % d;
-        } else {
-            ans = x - s0;
-        }
+    if d < 0 {
+        a = a + d * (n - 1);
+        d = -d;
     }
-    println!("{}", ans);
+    let s0 = a;
+    let sn = a + d * (n - 1);
+    if x >= s0 && x <= sn {
+        if d == 0 {
+            println!("0");
+        } else {
+            let ans = (x - a) % d;
+            println!("{}", ans.min(d - ans));
+        }
+    } else if x < s0 {
+        println!("{}", s0 - x);
+    } else {
+        println!("{}", x - sn);
+    }
 }
