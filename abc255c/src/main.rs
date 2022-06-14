99ba589dc2fcec0f64d60e9e03b21b32215a4e94
Wataru NAKAI <watnk0806@gmail.com>
2022-06-14 22:15:56 +0900

    ABC255 C; WA

--- abc255c/src/main.rs
+++ abc255c/src/main.rs
@@ -5,7 +5,27 @@ fn main() {
         x: i128,
         a: i128,
         d: i128,
-        n: usize
+        n: i128
     }
-    println!("{} {} {} {}", x, a, d, n);
+    let mut ans = 0;
+    let s0 = a;
+    let sn = a + (n - 1) * d;
+    if d > 0 {
+        if x < s0 {
+            ans = s0 - x;
+        } else if x < sn {
+            ans = (x - a) % d;
+        } else {
+            ans = x - sn;
+        }
+    } else {
+        if x < sn {
+            ans = sn - x;
+        } else if x < sn {
+            ans = (x - a) % d;
+        } else {
+            ans = x - s0;
+        }
+    }
+    println!("{}", ans);
 }
