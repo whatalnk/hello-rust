bacdafc607664e43f5b6fc260e713c3279dd2dc8
Wataru NAKAI <watnk0806@gmail.com>
2022-06-19 13:14:41 +0900

    ABC244 D; AC!

--- abc244d/src/main.rs
+++ abc244d/src/main.rs
@@ -5,6 +5,15 @@ fn main() {
         mut s: [char; 3],
         mut t: [char; 3]
     }
-    println!("{:?}", s);
-    println!("{:?}", t);
+    let mut cnt = 0;
+    for i in 0..3 {
+        if s[i] != t[i] {
+            cnt += 1;
+        }
+    }
+    if cnt == 2 {
+        println!("No");
+    } else {
+        println!("Yes");
+    }
 }
