0b93c8f34cf3fc0403d4bb72aac74734ed07ad8d
Wataru NAKAI <watnk0806@gmail.com>
2022-06-23 12:12:05 +0900

    ABC248 D; RE

--- abc248d/src/main.rs
+++ abc248d/src/main.rs
@@ -7,10 +7,19 @@ fn main() {
         a: [i64; n],
         q: usize
     }
-    let mut hm: HashMap<i64, Vec<usize>> = HashMap::new();
+    let mut v = vec![];
+    v.push(HashMap::<i64, i64>::new());
     for i in 0..n {
-        let e = hm.entry(a[i]).or_insert(vec![]);
-        e.push(i + 1);
+        if i == 0 {
+            let mut hm = HashMap::new();
+            hm.insert(a[i], 1i64);
+            v.push(hm);
+        } else {
+            let mut hm = v[i].clone();
+            let e = hm.entry(a[i]).or_insert(0);
+            *e += 1;
+            v.push(hm);
+        }
     }
     for _ in 0..q {
         input! {
@@ -18,26 +27,14 @@ fn main() {
             r: usize,
             x: i64
         }
-        // println!("{} {} {}", l, r, x);
-        if hm.contains_key(&x) {
-            let aa = hm.get(&x).unwrap();
-            if r < *aa.first().unwrap() || l > *aa.last().unwrap() {
-                println!("0");
-            } else {
-                // println!("{:?}", aa);
-                let li = match aa.binary_search(&l) {
-                    Ok(v) => v,
-                    Err(v) => v,
-                };
-                let ri = match aa.binary_search(&r) {
-                    Ok(v) => v,
-                    Err(v) => v,
-                };
-                // println!("{} {}", li, ri);
-                println!("{}", ri - li);
-            }
-        } else {
-            println!("0");
+        let mut right = 0;
+        if let Some(i) = v[r].get(&x) {
+            right = *i;
         }
+        let mut left = 0;
+        if let Some(i) = v[l - 1].get(&x) {
+            left = *i;
+        }
+        println!("{}", right - left);
     }
 }
