9ad8e796a9b700b6465fa3e869d553a83bc20389
Wataru NAKAI <watnk0806@gmail.com>
2022-06-24 21:32:34 +0900

    ABC248 D; WA

--- abc248d/src/main.rs
+++ abc248d/src/main.rs
@@ -1,40 +1,29 @@
 use proconio::input;
-use std::collections::HashMap;
 
 fn main() {
     input! {
         n: usize,
-        a: [i64; n],
+        a: [usize; n],
         q: usize
     }
-    let mut v = vec![];
-    v.push(HashMap::<i64, i64>::new());
+    let mut idx = vec![vec![]; n + 1];
     for i in 0..n {
-        if i == 0 {
-            let mut hm = HashMap::new();
-            hm.insert(a[i], 1i64);
-            v.push(hm);
-        } else {
-            let mut hm = v[i].clone();
-            let e = hm.entry(a[i]).or_insert(0);
-            *e += 1;
-            v.push(hm);
-        }
+        idx[a[i]].push(i);
     }
     for _ in 0..q {
         input! {
             l: usize,
             r: usize,
-            x: i64
+            x: usize
         }
-        let mut right = 0;
-        if let Some(i) = v[r].get(&x) {
-            right = *i;
-        }
-        let mut left = 0;
-        if let Some(i) = v[l - 1].get(&x) {
-            left = *i;
-        }
+        let right = match idx[x].binary_search(&(r - 1)) {
+            Ok(v) => v,
+            Err(v) => v,
+        };
+        let left = match idx[x].binary_search(&(l - 1)) {
+            Ok(v) => v,
+            Err(v) => v,
+        };
         println!("{}", right - left);
     }
 }
