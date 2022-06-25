13a140ff52e0169459c9b2b5f31111118ae41334
Wataru NAKAI <watnk0806@gmail.com>
2022-06-26 06:01:44 +0900

    ABC257 A; AC!

---
+++ abc257a/src/main.rs
@@ -0,0 +1,15 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        n: usize,
+        x: usize
+    }
+    let c: u8 = 'A' as u8;
+    let ans = (c - 1) + (x / n) as u8;
+    if x % n == 0 {
+        println!("{}", ans as char);
+    } else {
+        println!("{}", (ans + 1) as char);
+    }
+}
