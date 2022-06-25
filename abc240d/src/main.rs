a401759ef4b29de0f5aa0cb52ad4e55a9035e413
Wataru NAKAI <watnk0806@gmail.com>
2022-06-25 14:53:44 +0900

    ABC240 D; AC!

--- abc240d/src/main.rs
+++ abc240d/src/main.rs
@@ -1,10 +1,42 @@
 use proconio::input;
 
+struct S {
+    key: usize,
+    cnt: usize,
+}
+
 fn main() {
     input! {
         n: usize,
         a: [usize; n],
     }
-    println!("{}", n);
-    println!("{:?}", a);
+    let mut ans = 0;
+    let mut v: Vec<S> = vec![];
+    for i in 0..n {
+        if let Some(x) = v.pop() {
+            if x.key == a[i] {
+                let mut cur = S {
+                    key: a[i],
+                    cnt: x.cnt,
+                };
+                if cur.key == cur.cnt + 1 {
+                    ans -= cur.cnt;
+                } else {
+                    cur.cnt += 1;
+                    v.push(cur);
+                    ans += 1;
+                }
+            } else {
+                v.push(x);
+                let cur = S { key: a[i], cnt: 1 };
+                v.push(cur);
+                ans += 1;
+            }
+        } else {
+            let cur = S { key: a[i], cnt: 1 };
+            v.push(cur);
+            ans += 1;
+        }
+        println!("{}", ans);
+    }
 }
