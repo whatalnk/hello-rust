f31488f93da0758708b3d85ea4cc21488e18d492
Wataru NAKAI <watnk0806@gmail.com>
2022-06-28 11:46:26 +0900

    ABC257 C; WA

--- abc257c/src/main.rs
+++ abc257c/src/main.rs
@@ -5,9 +5,28 @@ fn main() {
     input! {
         n: usize,
         s: Chars,
-        w: [i64; n]
+        mut w: [i64; n]
     }
-    println!("{}", n);
-    println!("{:?}", s);
-    println!("{:?}", w);
+    let mut ws = vec![];
+    for i in 0..n {
+        ws.push((w[i], s[i]));
+    }
+    ws.sort();
+    // w.sort();
+    let mut ss = vec![];
+    ss.push(0);
+    ss.push(ws[0].1.to_digit(10).unwrap() as i64);
+    for i in 1..n {
+        ss.push(ss[i] + ws[i].1.to_digit(10).unwrap() as i64);
+    }
+    ss.push(ss[n - 1]);
+    let mut ans = 0;
+    for i in 0..=n {
+        let may_be_child = i as i64;
+        let may_be_adult = n as i64 - may_be_child;
+        let adult = ss[i];
+        let child = n as i64 - adult;
+        ans = ans.max(may_be_child.min(child) + may_be_adult.min(adult));
+    }
+    println!("{}", ans);
 }
