80a964b7c33bbe78d6eace7a6b946c9080e942fc
Wataru NAKAI <watnk0806@gmail.com>
2022-07-03 22:37:32 +0900

    ABC258 D; AC!

--- abc258d/src/main.rs
+++ abc258d/src/main.rs
@@ -1,6 +1,4 @@
 use proconio::input;
-use std::cmp::Reverse;
-use std::collections::BinaryHeap;
 
 fn main() {
     input! {
@@ -8,25 +6,14 @@ fn main() {
         x: usize,
         ab: [(i64, i64); n]
     }
-    let mut bh = BinaryHeap::new();
-    for i in 0..n {
-        bh.push((Reverse(ab[i].0 + ab[i].1), ab[i].0, ab[i].1));
-    }
-    let mut ans = 0;
-    for _ in 0..x {
-        if let Some(e) = bh.pop() {
-            match e.0 {
-                Reverse(p) => {
-                    ans += p;
-                    if p == e.1 + e.2 {
-                        bh.push((Reverse(e.2), e.1, e.1));
-                    } else {
-                        bh.push(e);
-                    }
-                }
-                _ => unreachable!(),
-            }
-        }
+    let mut aa = ab[0].0 + ab[0].1;
+    let mut bmin = ab[0].1;
+    let mut ans = aa + ((x - 1) as i64) * bmin;
+    for i in 1..(n.min(x)) {
+        aa += ab[i].0 + ab[i].1;
+        bmin = bmin.min(ab[i].1);
+        let ans_ = aa + ((x - i - 1).max(0) as i64) * bmin;
+        ans = ans.min(ans_);
     }
     println!("{}", ans);
 }
