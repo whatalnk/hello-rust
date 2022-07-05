f0512962d20e55c2f611a9a9100325e0919f8f8c
Wataru NAKAI <watnk0806@gmail.com>
2022-07-05 23:03:59 +0900

    ABC257 D; cont.

--- abc257d/src/main.rs
+++ abc257d/src/main.rs
@@ -1,12 +1,44 @@
 use proconio::input;
+use std::collections::VecDeque;
+
+struct S {
+    n: usize,
+    xyp: Vec<(i64, i64, i64)>,
+}
+impl S {
+    fn f(&self, st: usize, s: i64) -> bool {
+        let mut g = vec![vec![0; self.n]; self.n];
+        let mut done = vec![false; self.n];
+        let mut q = VecDeque::new();
+        q.push_back(st);
+        done[st] = true;
+        while q.len() > 0 {
+            let i = q.pop_front().unwrap();
+            for j in 0..self.n {
+                if g[i][j] == 0 && !done[j] {
+                    let (xi, yi, pi) = self.xyp[i];
+                    let (xj, yj, pj) = self.xyp[j];
+                    if s * pi - ((xi - xj).abs() + (yi - yj).abs()) >= 0 {
+                        g[i][j] = 1;
+                        done[j] = true;
+                        q.push_back(j);
+                    }
+                }
+            }
+        }
+        return done.iter().all(|&x| x);
+    }
+}
 
 fn main() {
     input! {
         n: usize,
-        xyp: [(isize, isize, i64); n]
+        xyp: [(i64, i64, i64); n]
     }
-    println!("{}", n);
+    let s = S { n: n, xyp: xyp };
     for i in 0..n {
-        println!("{:?}", xyp[i]);
+        for j in 0..5 {
+            println!("{} {} {}", i, j, s.f(i, j));
+        }
     }
 }
