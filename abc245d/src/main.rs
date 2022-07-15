6ce788f6ed65c2ad1d513fa2d537986c5342f7fa
Wataru NAKAI <watnk0806@gmail.com>
2022-07-15 23:18:32 +0900

    ABC245 D; cont.

--- abc245d/src/main.rs
+++ abc245d/src/main.rs
@@ -7,4 +7,21 @@ fn main() {
         a: [i64; n + 1],
         c: [i64; n + m + 1]
     }
+    let mut aa = vec![vec![0; n + m + 1]; m];
+    let mut d = 0;
+    for i in 0..(n + 1) {
+        for j in 0..(m + 1) {
+            aa[i][d + j] = a[i];
+        }
+        d += 1;
+    }
+    let mut b = vec![0; m + 1];
+    b[0] = c[0] / a[0];
+    for i in 0..(n + m + 1) {
+        for j in 0..m {
+            if i >= j && i <= m + j {
+                println!("{}: {}*{}", i, j, i - j)
+            }
+        }
+    }
 }
