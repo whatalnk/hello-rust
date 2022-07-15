4ec5dbbe4c6f47be0aee16aff1d6488e96e84ecd
Wataru NAKAI <watnk0806@gmail.com>
2022-07-16 00:14:58 +0900

    ABC245 D; WA

--- abc245d/src/main.rs
+++ abc245d/src/main.rs
@@ -7,21 +7,25 @@ fn main() {
         a: [i64; n + 1],
         c: [i64; n + m + 1]
     }
-    let mut aa = vec![vec![0; n + m + 1]; m];
-    let mut d = 0;
-    for i in 0..(n + 1) {
-        for j in 0..(m + 1) {
-            aa[i][d + j] = a[i];
-        }
-        d += 1;
-    }
     let mut b = vec![0; m + 1];
-    b[0] = c[0] / a[0];
+    b[m] = c[m + n] / a[n];
     for i in 0..(n + m + 1) {
+        let ii = n + m - i;
+        let mut cc = c[ii];
         for j in 0..m {
-            if i >= j && i <= m + j {
-                println!("{}: {}*{}", i, j, i - j)
+            if ii >= j && ii <= m + j {
+                cc -= a[j] * b[ii - j];
             }
         }
+        if i > 0 && i <= m {
+            b[m - i] = cc / a[n];
+        }
     }
+    println!(
+        "{}",
+        b.iter()
+            .map(|x| x.to_string())
+            .collect::<Vec<String>>()
+            .join(" ")
+    );
 }
