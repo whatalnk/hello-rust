2695a997ad9faf45f3e2af4da8fb3dd3fa7eedf2
Wataru NAKAI <watnk0806@gmail.com>
2022-07-16 13:53:22 +0900

    ABC245 D; AC...

--- abc245d/src/main.rs
+++ abc245d/src/main.rs
@@ -5,20 +5,14 @@ fn main() {
         n: usize,
         m: usize,
         a: [i64; n + 1],
-        c: [i64; n + m + 1]
+        mut c: [i64; n + m + 1]
     }
     let mut b = vec![0; m + 1];
-    b[m] = c[m + n] / a[n];
-    for i in 0..(n + m + 1) {
-        let ii = n + m - i;
-        let mut cc = c[ii];
-        for j in 0..m {
-            if ii >= j && ii <= m + j {
-                cc -= a[j] * b[ii - j];
-            }
-        }
-        if i > 0 && i <= m {
-            b[m - i] = cc / a[n];
+    for i in 0..=m {
+        let ii = m - i;
+        b[ii] = c[ii + n] / a[n];
+        for j in 0..=n {
+            c[ii + j] -= b[ii] * a[j];
         }
     }
     println!(
