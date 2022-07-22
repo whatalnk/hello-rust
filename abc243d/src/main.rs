c76c37f06fb30285ccdeba09c74cda21c7616b9a
Wataru NAKAI <watnk0806@gmail.com>
2022-07-22 22:47:25 +0900

    ABC243 D; WA

--- abc243d/src/main.rs
+++ abc243d/src/main.rs
@@ -4,8 +4,22 @@ use proconio::marker::Chars;
 fn main() {
     input! {
         n: usize,
-        x: usize,
+        mut x: i64,
         s: Chars
     }
-    println!("{} {} {:?}", n, x, s);
+    for i in 0..n {
+        if s[i] == 'U' {
+            if x % 2 == 0 {
+                x /= 2;
+            } else {
+                x = (x - 1) / 2;
+            }
+        } else if s[i] == 'L' {
+            x *= 2;
+        } else {
+            // R
+            x = x * 2 + 1;
+        }
+    }
+    println!("{}", x);
 }
