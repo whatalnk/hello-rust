0607d845395f73fcdf1735b24aa383064cf012ee
Wataru NAKAI <watnk0806@gmail.com>
2022-07-23 23:14:20 +0900

    ABC261B; AC!

---
+++ abc261b/src/main.rs
@@ -0,0 +1,32 @@
+use proconio::input;
+use proconio::marker::Chars;
+
+fn main() {
+    input! {
+        n: usize,
+        a: [Chars; n],
+    }
+    for i in 0..n {
+        for j in 0..n {
+            if i == j {
+                if a[i][j] != '-' {
+                    println!("incorrect");
+                    return;
+                } else {
+                    continue;
+                }
+            } else {
+                if (a[i][j] == 'D' && a[j][i] == 'D')
+                    || (a[i][j] == 'W' && a[j][i] == 'L')
+                    || (a[i][j] == 'L' && a[j][i] == 'W')
+                {
+                    continue;
+                } else {
+                    println!("incorrect");
+                    return;
+                }
+            }
+        }
+    }
+    println!("correct");
+}
