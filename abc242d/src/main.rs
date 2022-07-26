3e9cd6159f90b64d708f45927a2e1da62e658969
Wataru NAKAI <watnk0806@gmail.com>
2022-07-27 00:09:14 +0900

    ABC242 D; WA

--- abc242d/src/main.rs
+++ abc242d/src/main.rs
@@ -1,5 +1,6 @@
 use proconio::input;
 use proconio::marker::Chars;
+use std::collections::HashMap;
 
 fn main() {
     input! {
@@ -7,7 +8,33 @@ fn main() {
         q: usize,
         tk: [(usize, usize); q]
     }
-    println!("{:?}", s);
-    println!("{}", q);
-    println!("{:?}", tk);
+    let mut hm = HashMap::new();
+    hm.insert('A', ['B', 'C']);
+    hm.insert('B', ['C', 'A']);
+    hm.insert('C', ['A', 'B']);
+    for i in 0..q {
+        let (t, mut k) = tk[i];
+        k -= 1;
+        let mut cnt = 0;
+        let mut lr = vec![];
+        while cnt < t {
+            if k % 2 == 0 {
+                lr.push(0);
+            } else {
+                lr.push(1);
+            }
+            k /= 2;
+            cnt += 1;
+            if k == 0 {
+                break;
+            }
+        }
+        lr.reverse();
+        let mut ans = s[k];
+        for j in 0..cnt {
+            ans = hm.get(&s[k]).unwrap()[lr[j]];
+        }
+
+        println!("{}", ans);
+    }
 }
