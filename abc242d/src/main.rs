61049af1bc683c78d7339f7b507f3d48e0c988f9
Wataru NAKAI <watnk0806@gmail.com>
2022-07-28 22:30:33 +0900

    ABC242 D; AC...

--- abc242d/src/main.rs
+++ abc242d/src/main.rs
@@ -1,40 +1,38 @@
 use proconio::input;
 use proconio::marker::Chars;
-use std::collections::HashMap;
+
+struct S {
+    s: Vec<char>,
+}
+
+impl S {
+    fn g(&self, s: char, add: i64) -> char {
+        let a = 'A' as u8;
+        let s_ = (s as u8) as i64;
+        let offset = ((s_ - (a as i64) + add) % 3) as u8;
+        return (a + offset) as char;
+    }
+
+    fn f(&self, t: i64, k: i64) -> char {
+        if t == 0 {
+            return self.s[k as usize];
+        }
+        if k == 0 {
+            return self.g(self.s[0], t);
+        }
+        return self.g(self.f(t - 1, k / 2), k % 2 + 1);
+    }
+}
 
 fn main() {
     input! {
         s: Chars,
         q: usize,
-        tk: [(usize, usize); q]
+        tk: [(i64, i64); q]
     }
-    let mut hm = HashMap::new();
-    hm.insert('A', ['B', 'C']);
-    hm.insert('B', ['C', 'A']);
-    hm.insert('C', ['A', 'B']);
+    let ss = S { s };
     for i in 0..q {
-        let (t, mut k) = tk[i];
-        k -= 1;
-        let mut cnt = 0;
-        let mut lr = vec![];
-        while cnt < t {
-            if k % 2 == 0 {
-                lr.push(0);
-            } else {
-                lr.push(1);
-            }
-            k /= 2;
-            cnt += 1;
-            if k == 0 {
-                break;
-            }
-        }
-        lr.reverse();
-        let mut ans = s[k];
-        for j in 0..cnt {
-            ans = hm.get(&s[k]).unwrap()[lr[j]];
-        }
-
-        println!("{}", ans);
+        let (t, k) = tk[i];
+        println!("{}", ss.f(t, k - 1));
     }
 }
