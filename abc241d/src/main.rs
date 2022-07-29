2e01589736715f054f1ff7ed44ba17ff08120586
Wataru NAKAI <watnk0806@gmail.com>
2022-07-29 21:51:38 +0900

    ABC241 D; TLE

--- abc241d/src/main.rs
+++ abc241d/src/main.rs
@@ -1,22 +1,57 @@
 use proconio::input;
+use std::collections::BinaryHeap;
 
 fn main() {
     input!(n: usize);
-    println!("{}", n);
+    let mut a = BinaryHeap::new();
     for _ in 0..n {
         input!(c: u32);
         match c {
             1 => {
                 input!(x: i64);
-                println!("{}", x);
+                a.push(x);
             }
             2 => {
                 input!(x: i64, k: usize);
-                println!("{} {}", x, k);
+                let b = a.clone().into_sorted_vec();
+                let idx = b.binary_search(&x);
+                match idx {
+                    Ok(idx) => {
+                        if k <= idx {
+                            println!("{}", b[idx - k + 1]);
+                        } else {
+                            println!("-1");
+                        }
+                    }
+                    Err(idx) => {
+                        if k <= idx {
+                            println!("{}", b[idx - k]);
+                        } else {
+                            println!("-1");
+                        }
+                    }
+                }
             }
             _ => {
                 input!(x: i64, k: usize);
-                println!("{} {}", x, k);
+                let b = a.clone().into_sorted_vec();
+                let idx = b.binary_search(&x);
+                match idx {
+                    Ok(idx) => {
+                        if k <= b.len() - idx {
+                            println!("{}", b[idx + k - 1]);
+                        } else {
+                            println!("-1");
+                        }
+                    }
+                    Err(idx) => {
+                        if k <= b.len() - idx {
+                            println!("{}", b[idx + k - 1]);
+                        } else {
+                            println!("-1");
+                        }
+                    }
+                }
             }
         }
     }
