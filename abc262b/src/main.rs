af46867022e04d9eef036b78c98e6766a47c3eb4
Wataru NAKAI <watnk0806@gmail.com>
2022-08-02 22:00:56 +0900

    ABC262 B; AC!

---
+++ abc262b/src/main.rs
@@ -0,0 +1,26 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        n: usize,
+        m: usize,
+        uv: [(usize, usize); m]
+    }
+    let mut g = vec![vec![false; n]; n];
+    for i in 0..m {
+        let (u, v) = uv[i];
+        g[u - 1][v - 1] = true;
+        g[v - 1][u - 1] = true;
+    }
+    let mut ans = 0;
+    for i in 0..n {
+        for j in (i + 1)..n {
+            for k in (j + 1)..n {
+                if g[i][j] && g[j][k] && g[k][i] {
+                    ans += 1;
+                }
+            }
+        }
+    }
+    println!("{}", ans);
+}
