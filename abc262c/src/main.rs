a7a57b8b58fbabf66597a554301c93de348f545c
Wataru NAKAI <watnk0806@gmail.com>
2022-08-04 22:37:10 +0900

    ABC262 C; AC!

--- abc262c/src/main.rs
+++ abc262c/src/main.rs
@@ -6,12 +6,22 @@ fn main() {
         a: [i64; n]
     }
     let mut ans: i64 = 0;
+    let mut b: Vec<i64> = vec![0; n];
     for i in 0..n {
-        for j in (i + 1)..n {
-            let ii = (i + 1) as i64;
-            let jj = (j + 1) as i64;
-            if (ii, jj) == (a[i], a[j]) || (ii, jj) == (a[j], a[i]) {
-                println!("{} {}", ii, jj);
+        if a[i] == (i + 1) as i64 {
+            b[i] = 1;
+        }
+    }
+    let mut c: Vec<i64> = vec![0; n];
+    c[0] = b[0];
+    for i in 1..n {
+        c[i] = c[i - 1] + b[i];
+    }
+    for i in 0..n {
+        if a[i] == (i + 1) as i64 {
+            ans += c[n - 1] - c[i];
+        } else {
+            if i < (a[i] - 1) as usize && a[(a[i] - 1) as usize] == (i + 1) as i64 {
                 ans += 1;
             }
         }
