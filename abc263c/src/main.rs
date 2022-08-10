95e08ba59c6aeb125680ba9883a7ef9aaac6f9fe
Wataru NAKAI <watnk0806@gmail.com>
2022-08-10 20:50:09 +0900

    ABC263 C; WA

--- abc263c/src/main.rs
+++ abc263c/src/main.rs
@@ -1,9 +1,22 @@
 use proconio::input;
 
+fn f(n: usize, m: usize, cnt: usize, cur: usize, s: String) {
+    if cnt < n {
+        for i in (cur + 1)..=m {
+            let s_ = "".to_string() + &s + &" ".to_string() + &i.to_string();
+            f(n, m, cnt + 1, i, s_);
+        }
+    } else {
+        println!("{}", s);
+    }
+}
+
 fn main() {
     input! {
         n: usize,
-        m: i64,
+        m: usize,
     }
-    println!("{} {}", n, m);
+    for i in 1..=n {
+        f(n, m, 1, i, i.to_string());
+    }
 }
