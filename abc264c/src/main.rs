ef7182d7932b4212943a4e2f23d1d424a9ef0d0f
Wataru NAKAI <watnk0806@gmail.com>
2022-08-16 20:41:35 +0900

    ABC264 C; AC!

--- abc264c/src/main.rs
+++ abc264c/src/main.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
@@ -9,8 +10,24 @@ fn main() {
         w2: usize,
         b: [[i64; w2]; h2]
     }
-    println!("{} {}", h1, w1);
-    println!("{:?}", a);
-    println!("{} {}", h2, w2);
-    println!("{:?}", b);
+    let ith = (0..h1).combinations(h2).collect::<Vec<_>>();
+    let itw = (0..w1).combinations(w2).collect::<Vec<_>>();
+    for ih in 0..ith.len() {
+        for iw in 0..itw.len() {
+            let mut ans = true;
+            'outer: for i in 0..h2 {
+                for j in 0..w2 {
+                    if a[ith[ih][i]][itw[iw][j]] != b[i][j] {
+                        ans = false;
+                        break 'outer;
+                    }
+                }
+            }
+            if ans {
+                println!("Yes");
+                return;
+            }
+        }
+    }
+    println!("No");
 }
