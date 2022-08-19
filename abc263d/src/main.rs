0f0baef61a6487592b9031b38e2a29490eb041ee
Wataru NAKAI <watnk0806@gmail.com>
2022-08-19 21:24:28 +0900

    ABC263 D; AC...

--- abc263d/src/main.rs
+++ abc263d/src/main.rs
@@ -3,10 +3,21 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        l: usize,
-        r: usize,
+        l: i64,
+        r: i64,
         a: [i64; n],
     }
-    println!("{} {} {}", n, l, r);
-    println!("{:?}", a);
+    let mut f = vec![0i64; n + 1];
+    let mut g = vec![0i64; n + 1];
+    for i in 1..=n {
+        f[i] = (f[i - 1] + a[i - 1]).min(l * (i as i64));
+    }
+    for i in 1..=n {
+        g[n - i] = (g[n - i + 1] + a[n - i]).min(r * (i as i64));
+    }
+    let mut ans = f[0] + g[0];
+    for i in 0..=n {
+        ans = ans.min(f[i] + g[i]);
+    }
+    println!("{}", ans);
 }
