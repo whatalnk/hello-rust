7ab6a8c6fef9f5e8c8099e2249f8bc6c6350c8c4
Wataru NAKAI <watnk0806@gmail.com>
2022-08-23 22:33:34 +0900

    ABC265 B; AC!

--- abc265b/src/main.rs
+++ abc265b/src/main.rs
@@ -8,15 +8,15 @@ fn main() {
         a: [i64; n-1],
         xy: [(usize, i64); m],
     }
-    let mut b = vec![0; n];
+    let mut b = vec![0; n + 1];
     for i in 0..m {
         let (x, y) = xy[i];
-        b[x - 1] = y;
+        b[x] = y;
     }
     let mut ans = true;
-    for i in 0..(n - 1) {
-        if t >= a[i] {
-            t -= a[i];
+    for i in 1..=(n - 1) {
+        if t > a[i - 1] {
+            t -= a[i - 1];
             t += b[i + 1];
         } else {
             ans = false;
