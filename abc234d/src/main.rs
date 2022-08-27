9e00ea123ca832d8f2611421f980b64bd287848d
Wataru NAKAI <watnk0806@gmail.com>
2022-08-27 16:37:46 +0900

    ABC234 D; AC!

--- abc234d/src/main.rs
+++ abc234d/src/main.rs
@@ -1,11 +1,26 @@
 use proconio::input;
+use std::collections::BTreeSet;
+use std::ops::Bound::Included;
 
 fn main() {
     input! {
         n: usize,
-        _: usize,
+        k: usize,
         p: [i64; n]
     }
-    println!("{}", n);
-    println!("{:?}", p);
+    let mut bts = BTreeSet::new();
+    let mut vk: i64 = n as i64;
+    for i in 0..k {
+        vk = vk.min(p[i]);
+        bts.insert(p[i]);
+    }
+    println!("{}", vk);
+    for i in k..n {
+        bts.insert(p[i]);
+        if vk < p[i] {
+            let mut it = bts.range((Included(&(vk + 1)), Included(&p[i])));
+            vk = *it.next().unwrap();
+        }
+        println!("{}", vk);
+    }
 }
