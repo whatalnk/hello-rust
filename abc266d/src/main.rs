a877c6c4249957f71a0fdec555f4e7f4ebd9d44f
Wataru NAKAI <watnk0806@gmail.com>
2022-09-06 22:54:19 +0900

    ABC266 D; AC...

--- abc266d/src/main.rs
+++ abc266d/src/main.rs
@@ -6,32 +6,31 @@ fn main() {
         txa: [(usize, usize, i64); n],
     }
     let t_max = 100001;
-    let mut tx = vec![vec![0; t_max]; 5];
+    let minimum = -1_000_000_000_000_000_000i64;
+    let mut xx = vec![0; t_max];
+    let mut aa = vec![0; t_max];
     for i in 0..n {
         let (t, x, a) = txa[i];
-        tx[x][t] = a;
+        xx[t] = x;
+        aa[t] = a;
     }
-    let mut g = vec![vec![0; t_max]; 5];
-    let mut mask = vec![vec![1; t_max]; 5];
-    mask[2][1] = 0;
-    mask[3][1] = 0;
-    mask[4][1] = 0;
-    mask[3][2] = 0;
-    mask[4][2] = 0;
-    mask[4][3] = 0;
-    for j in 1..t_max {
+    let mut g = vec![vec![minimum; t_max]; 5];
+    g[0][0] = 0;
+    for t in 1..t_max {
         for i in 0..5 {
-            if i == 0 {
-                g[i][j] = (g[i][j - 1] * mask[i][j - 1]).max(g[i + 1][j - 1] * mask[i + 1][j - 1]);
-            } else if i == 4 {
-                g[i][j] = (g[i][j - 1] * mask[i][j - 1]).max(g[i - 1][j - 1] * mask[i - 1][j - 1]);
-            } else {
-                g[i][j] = (g[i - 1][j - 1] * mask[i - 1][j - 1])
-                    .max(g[i][j - 1] * mask[i][j - 1])
-                    .max(g[i + 1][j - 1] * mask[i + 1][j - 1]);
+            g[i][t] = g[i][t - 1];
+            if i != 0 {
+                g[i][t] = g[i][t].max(g[i - 1][t - 1]);
             }
-            g[i][j] += tx[i][j] * mask[i][j];
+            if i != 4 {
+                g[i][t] = g[i][t].max(g[i + 1][t - 1]);
+            }
         }
+        g[xx[t]][t] += aa[t];
     }
-    println!("{}", g[4][t_max - 1])
+    let mut ans = 0;
+    for i in 0..5 {
+        ans = ans.max(g[i][t_max - 1]);
+    }
+    println!("{}", ans);
 }
