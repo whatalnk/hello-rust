69755d714fac5ddeca45aa49c20df2e0c7783055
Wataru NAKAI <watnk0806@gmail.com>
2022-09-09 22:09:21 +0900

    ABC267 D; AC...

--- abc267d/src/main.rs
+++ abc267d/src/main.rs
@@ -6,6 +6,19 @@ fn main() {
         m: usize,
         a: [i64; n],
     }
-    println!("{} {}", n, m);
-    println!("{:?}", a);
+    let minimum = -1000000000000000000;
+    let mut dp = vec![vec![minimum; 2001]; 2001];
+    dp[0][0] = 0i64;
+    for i in 1..=n {
+        for j in 0..=n {
+            if j == 0 {
+                dp[i][0] = 0;
+            } else if j > i {
+                dp[i][j] = minimum;
+            } else {
+                dp[i][j] = dp[i - 1][j].max(dp[i - 1][j - 1] + a[i - 1] * (j as i64));
+            }
+        }
+    }
+    println!("{}", dp[n][m]);
 }
