adce645f748d6cbaa14a756dc8911d6ae0f20dab
Wataru NAKAI <watnk0806@gmail.com>
2022-09-14 10:45:37 +0900

    ABC268 D; AC...

--- abc268d/src/main.rs
+++ abc268d/src/main.rs
@@ -1,17 +1,58 @@
+use itertools::Itertools;
 use proconio::input;
+use std::process::exit;
+
+fn contain_ans(ans: &String, t: &Vec<String>) -> bool {
+    let x = t.binary_search(ans);
+    if x.is_ok() {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+fn dfs(cur: usize, s: &Vec<String>, t: &Vec<String>, remain: i64, ans: String) {
+    if remain < 0 {
+        return;
+    }
+    if cur == s.len() {
+        if ans.len() >= 3 && !contain_ans(&ans, t) {
+            println!("{}", ans);
+            exit(0);
+        }
+        return;
+    }
+    if ans.len() > 0 && (!ans.ends_with("_")) {
+        let mut ans_ = ans.clone();
+        ans_.push('_');
+        dfs(cur, s, t, remain, ans_);
+    } else {
+        let mut ans_ = ans.clone();
+        ans_.push_str(&s[cur]);
+        dfs(cur + 1, s, t, remain, ans_);
+        if ans.len() > 0 {
+            let mut ans_ = ans.clone();
+            ans_.push('_');
+            dfs(cur, s, t, remain - 1, ans_);
+        }
+    }
+}
 
 fn main() {
     input! {
         n: usize,
         m: usize,
-        ss: [String; n],
-        tt: [String; m],
+        mut ss: [String; n],
+        mut tt: [String; m],
     }
-    println!("{} {}", n, m);
+    tt.sort();
+    let mut remain = 16;
     for i in 0..n {
-        println!("{}", ss[i]);
+        remain = remain - (ss[i].len() as i64);
     }
-    for i in 0..m {
-        println!("{}", tt[i]);
-    }
+    remain = remain - ((n - 1) as i64);
+    ss.into_iter()
+        .permutations(n)
+        .for_each(|perm| dfs(0, &perm, &tt, remain, "".to_string()));
+    println!("-1");
 }
