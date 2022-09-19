be6d465963494cd399d269caf74cde0cb38efa78
Wataru NAKAI <watnk0806@gmail.com>
2022-09-20 00:14:38 +0900

    ABC269 D; AC!

--- abc269d/src/main.rs
+++ abc269d/src/main.rs
@@ -1,12 +1,39 @@
 use proconio::input;
+use std::collections::HashSet;
 
 fn main() {
     input! {
         n: usize,
         xy: [(i64, i64); n]
     }
-    println!("{}", n);
+    let dx = vec![-1, -1, 0, 0, 1, 1];
+    let dy = vec![-1, 0, -1, 1, 0, 1];
+
+    let mut black = HashSet::<(i64, i64)>::new();
+    let mut visited = HashSet::<(i64, i64)>::new();
+
     for i in 0..n {
-        println!("{} {}", xy[i].0, xy[i].1);
+        black.insert(xy[i]);
     }
+    let mut ans = 0;
+    for i in 0..n {
+        if !visited.contains(&xy[i]) {
+            ans += 1;
+            let mut q = vec![];
+            q.push(xy[i]);
+            while q.len() > 0 {
+                if let Some((x, y)) = q.pop() {
+                    visited.insert((x, y));
+                    for d in 0..6 {
+                        let nx = x + dx[d];
+                        let ny = y + dy[d];
+                        if black.contains(&(nx, ny)) && !visited.contains(&(nx, ny)) {
+                            q.push((nx, ny));
+                        }
+                    }
+                }
+            }
+        }
+    }
+    println!("{}", ans);
 }
