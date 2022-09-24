56cbfa7c4e6000fb293742297296eb32bd0f071f
Wataru NAKAI <watnk0806@gmail.com>
2022-09-24 21:05:55 +0900

    ABC233 D; AC...

--- abc233d/src/main.rs
+++ abc233d/src/main.rs
@@ -1,4 +1,5 @@
 use proconio::input;
+use std::collections::HashMap;
 
 fn main() {
     input! {
@@ -6,6 +7,18 @@ fn main() {
         k: i64,
         a: [i64; n],
     }
-    println!("{} {}", n, k);
-    println!("{:?}", a);
+    let mut b = vec![0; n + 1];
+    for i in 0..n {
+        b[i + 1] = b[i] + a[i];
+    }
+    let mut ans = 0i64;
+    let mut hm = HashMap::new();
+    for r in 1..=n {
+        let e = hm.entry(&b[r - 1]).or_insert(0);
+        *e += 1;
+        if let Some(e) = hm.get(&(b[r] - k)) {
+            ans += e;
+        };
+    }
+    println!("{}", ans);
 }
