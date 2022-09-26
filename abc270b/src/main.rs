078d3c350ce52a89fe46c3d8b7fe3a3d6af5b68f
Wataru NAKAI <watnk0806@gmail.com>
2022-09-26 21:37:01 +0900

    ABC270 B; AC!

---
+++ abc270b/src/main.rs
@@ -0,0 +1,38 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        x: i64,
+        y: i64,
+        z: i64,
+    }
+    if x > 0 && y > 0 {
+        if x < y {
+            println!("{}", x);
+        } else {
+            if z > y {
+                println!("-1");
+            } else if z < 0 {
+                println!("{}", x + z.abs() * 2);
+            } else {
+                println!("{}", x);
+            }
+        }
+    } else if x > 0 && y < 0 {
+        println!("{}", x);
+    } else if x < 0 && y > 0 {
+        println!("{}", x.abs());
+    } else {
+        if y < x {
+            println!("{}", x.abs());
+        } else {
+            if z < y {
+                println!("-1");
+            } else if z > 0 {
+                println!("{}", x.abs() + z * 2);
+            } else {
+                println!("{}", x.abs());
+            }
+        }
+    }
+}
