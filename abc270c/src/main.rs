1e21635289f7a9c9b65a739b7943624710a92578
Wataru NAKAI <watnk0806@gmail.com>
2022-09-27 23:57:50 +0900

    ABC270 C; AC!

---
+++ abc270c/src/main.rs
@@ -0,0 +1,52 @@
+use itertools::Itertools;
+use proconio::input;
+
+fn main() {
+    input! {
+        n: usize,
+        x: usize,
+        y: usize,
+        uv: [(usize, usize); n-1]
+    }
+    // bfs
+    let mut g = vec![vec![]; n + 1];
+    for i in 0..(n - 1) {
+        let (u, v) = uv[i];
+        g[u].push(v);
+        g[v].push(u);
+    }
+
+    let inf = 1000000000i64;
+    let mut d = vec![inf; n + 1];
+    d[x] = 0;
+    let mut q = vec![];
+    let mut prev = vec![-1; n + 1];
+    q.push(x);
+    while q.len() > 0 {
+        if let Some(p) = q.pop() {
+            if p == y {
+                break;
+            }
+            for i in 0..g[p].len() {
+                if d[g[p][i]] == inf {
+                    q.push(g[p][i]);
+                    d[g[p][i]] = d[p] + 1;
+                    prev[g[p][i]] = p as isize;
+                }
+            }
+        }
+    }
+    let mut path = vec![];
+    path.push(y as isize);
+    let mut t = prev[y];
+    path.push(t);
+    loop {
+        t = prev[t as usize];
+        if t == -1 {
+            break;
+        }
+        path.push(t);
+    }
+    path.reverse();
+    println!("{}", path.iter().map(|e| e.to_string()).join(" "));
+}
