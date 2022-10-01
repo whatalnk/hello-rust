9a55da6f55ba9017b5ef9637fe53a785cb25802f
Wataru NAKAI <watnk0806@gmail.com>
2022-10-01 22:49:03 +0900

    ABC232 D; AC...

--- abc232d/src/main.rs
+++ abc232d/src/main.rs
@@ -1,28 +1,5 @@
 use proconio::input;
 use proconio::marker::Chars;
-use std::cell::Cell;
-
-struct S {
-    h: usize,
-    w: usize,
-    c: Vec<Vec<char>>,
-    ans: Cell<i64>,
-}
-
-impl S {
-    fn dfs(&self, i: usize, j: usize, cnt: i64) {
-        let d = vec![(0, 1), (1, 0)];
-        for di in 0..2 {
-            let ni = i + d[di].0;
-            let nj = j + d[di].1;
-            if ni < self.h && nj < self.w && self.c[ni][nj] != '#' {
-                self.dfs(ni, nj, cnt + 1);
-            } else {
-                self.ans.set(cnt.max(self.ans.get()));
-            }
-        }
-    }
-}
 
 fn main() {
     input! {
@@ -30,12 +7,15 @@ fn main() {
         w: usize,
         c: [Chars; h],
     }
-    let s = S {
-        h: h,
-        w: w,
-        c: c,
-        ans: Cell::new(0),
-    };
-    s.dfs(0, 0, 1);
-    println!("{}", s.ans.get());
+    let mut f = vec![vec![0; w + 1]; h + 1];
+    for i in 0..h {
+        for j in 0..w {
+            let ii = h - i - 1;
+            let jj = w - j - 1;
+            if c[ii][jj] != '#' {
+                f[ii][jj] = f[ii + 1][jj].max(f[ii][jj + 1]) + 1;
+            }
+        }
+    }
+    println!("{}", f[0][0]);
 }
