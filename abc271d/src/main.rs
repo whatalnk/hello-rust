77044477091ff018f2b542cc6505493cd84afe92
Wataru NAKAI <watnk0806@gmail.com>
2022-10-06 22:50:54 +0900

    ABC271 D; AC...

--- abc271d/src/main.rs
+++ abc271d/src/main.rs
@@ -3,9 +3,38 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        s: i64,
-        ab: [(i64, i64); n],
+        mut s: usize,
+        ab: [(usize, usize); n],
     }
-    println!("{} {}", n, s);
-    println!("{:?}", ab);
+    let mut dp = vec![vec![false; s + 1]; n + 1];
+    dp[0][0] = true;
+    for i in 0..n {
+        for j in 0..=s {
+            if dp[i][j] {
+                if j + ab[i].0 <= s {
+                    dp[i + 1][j + ab[i].0] = true;
+                }
+                if j + ab[i].1 <= s {
+                    dp[i + 1][j + ab[i].1] = true;
+                }
+            }
+        }
+    }
+    if dp[n][s] {
+        println!("Yes");
+        let mut t = vec!['H'; n];
+        for i in 0..n {
+            let ii = n - 1 - i;
+            if s >= ab[ii].0 && dp[ii][s - ab[ii].0] {
+                t[ii] = 'H';
+                s -= ab[ii].0;
+            } else {
+                t[ii] = 'T';
+                s -= ab[ii].1;
+            }
+        }
+        println!("{}", t.into_iter().collect::<String>());
+    } else {
+        println!("No");
+    }
 }
