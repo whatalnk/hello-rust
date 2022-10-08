5c3cd97195a098eb58f1cd5eb28eb449acfc26d9
Wataru NAKAI <watnk0806@gmail.com>
2022-10-08 22:21:04 +0900

    ABC231 D; AC...

--- abc231d/src/main.rs
+++ abc231d/src/main.rs
@@ -1,5 +1,43 @@
 use proconio::input;
-use std::collections::HashSet;
+
+struct UnionFind {
+    par: Vec<usize>,
+    rank: Vec<i64>,
+}
+impl UnionFind {
+    fn new(n: usize) -> UnionFind {
+        UnionFind {
+            par: (0..n).collect::<Vec<usize>>(),
+            rank: vec![0; n],
+        }
+    }
+    fn find(&mut self, x: usize) -> usize {
+        if self.par[x] == x {
+            return x;
+        } else {
+            self.par[x] = self.find(self.par[x]);
+            return self.par[x];
+        }
+    }
+    fn unite(&mut self, mut x: usize, mut y: usize) {
+        x = self.find(x);
+        y = self.find(y);
+        if x == y {
+            return;
+        }
+        if self.rank[x] < self.rank[y] {
+            self.par[x] = y;
+        } else {
+            self.par[y] = x;
+            if self.rank[x] == self.rank[y] {
+                self.rank[x] += 1;
+            }
+        }
+    }
+    fn same(&mut self, x: usize, y: usize) -> bool {
+        return self.find(x) == self.find(y);
+    }
+}
 
 fn main() {
     input! {
@@ -7,12 +45,22 @@ fn main() {
         m: usize,
         ab: [(usize, usize); m],
     }
-    let mut v = vec![HashSet::new(); n + 1];
+    let mut d = vec![0; n];
+    let mut uf = UnionFind::new(n);
     for i in 0..m {
-        let (a, b) = ab[i];
-        v[a].insert(b);
-        v[b].insert(a);
-        if v[a].len() > 2 || v[b].len() > 2 {
+        let (mut a, mut b) = ab[i];
+        a -= 1;
+        b -= 1;
+        if uf.same(a, b) {
+            println!("No");
+            return;
+        }
+        uf.unite(a, b);
+        d[a] += 1;
+        d[b] += 1;
+    }
+    for i in 0..n {
+        if d[i] > 2 {
             println!("No");
             return;
         }
