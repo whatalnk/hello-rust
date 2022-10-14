2f34272b6254bf83f8df79a3703aa9f247b90c5f
Wataru NAKAI <watnk0806@gmail.com>
2022-10-14 23:11:24 +0900

    ABC230 D; AC...

--- abc230d/src/main.rs
+++ abc230d/src/main.rs
@@ -3,12 +3,18 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        d: usize,
-        lr: [(usize, usize); n]
+        d: i64,
+        mut lr: [(i64, i64); n]
     }
-    println!("{} {}", n, d);
+    lr.sort_by(|a, b| a.1.cmp(&b.1));
+    let mut ans = 0;
+    let mut x = -(1i64 << 40);
     for i in 0..n {
         let (l, r) = lr[i];
-        println!("{} {}", l, r);
+        if x + d - 1 < l {
+            ans += 1;
+            x = r;
+        }
     }
+    println!("{}", ans);
 }
