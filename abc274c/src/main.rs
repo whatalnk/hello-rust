38db2329d2b1011666511ddb702e0460e642052e
Wataru NAKAI <watnk0806@gmail.com>
2022-10-24 21:38:19 +0900

    ABC274 C; AC!

---
+++ abc274c/src/main.rs
@@ -0,0 +1,21 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        n: usize,
+        a: [usize; n]
+    }
+    let mut p = vec![0; 2 * (n + 1)];
+    let mut d = vec![0; 2 * (n + 1)];
+    for i in 0..n {
+        let l = 2 * (i + 1);
+        let r = 2 * (i + 1) + 1;
+        p[l] = a[i];
+        p[r] = a[i];
+        d[l] = d[a[i]] + 1;
+        d[r] = d[a[i]] + 1;
+    }
+    for k in 1..=(2 * n + 1) {
+        println!("{}", d[k]);
+    }
+}
