22f4874f3c513c4af5aa0b744234ede8ba04f7e2
Wataru NAKAI <watnk0806@gmail.com>
2022-10-26 22:36:09 +0900

    ABC273 D; RE

--- abc273d/src/main.rs
+++ abc273d/src/main.rs
@@ -37,18 +37,23 @@ fn main() {
             if let Some(v) = hmr.get(&rs) {
                 let j = v.binary_search(&cs).unwrap_err();
                 match j {
-                    0 => ce = (ce - l).max(1),
+                    0 if ce > l => ce = ce - l,
+                    0 => ce = 1,
                     _ if j == v.len() => ce = (ce - l).max(v[j - 1] + 1),
                     _ => ce = (ce - l).max(v[j] + 1),
                 }
             } else {
-                ce = (ce - l).max(1);
+                if ce > l {
+                    ce = ce - l;
+                } else {
+                    ce = 1;
+                }
             }
         } else if d == 'R' {
             if let Some(v) = hmr.get(&rs) {
                 let j = v.binary_search(&cs).unwrap_err();
-                println!("v={:?}, cs={}, j={}", v, cs, j);
                 match j {
+                    0 => ce = (ce + l).min(v[j] - 1),
                     _ if j == v.len() => ce = (ce + l).min(w),
                     _ => ce = (ce + l).min(v[j + 1] - 1),
                 }
@@ -59,17 +64,23 @@ fn main() {
             if let Some(v) = hmc.get(&cs) {
                 let j = v.binary_search(&rs).unwrap_err();
                 match j {
-                    0 => re = (re - l).max(1),
+                    0 if re > l => re = re - l,
+                    0 => re = 1,
                     _ if j == v.len() => re = (re - l).max(v[j - 1] + 1),
                     _ => re = (re - l).max(v[j] + 1),
                 }
             } else {
-                re = (re - l).max(1);
+                if re > l {
+                    re = re - l;
+                } else {
+                    re = 1;
+                }
             }
         } else {
             if let Some(v) = hmc.get(&cs) {
                 let j = v.binary_search(&rs).unwrap_err();
                 match j {
+                    0 => re = (re + l).min(v[j] - 1),
                     _ if j == v.len() => re = (re + l).min(h),
                     _ => re = (re + l).min(v[j + 1] - 1),
                 }
@@ -78,6 +89,7 @@ fn main() {
             }
         }
         println!("{} {}", re, ce);
+
         rs = re;
         cs = ce;
     }
