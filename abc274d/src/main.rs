aecfe304831329e4c3ee029f19f233243ba73a84
Wataru NAKAI <watnk0806@gmail.com>
2022-10-27 23:50:06 +0900

    ABC274 D; cont.

--- abc274d/src/main.rs
+++ abc274d/src/main.rs
@@ -7,6 +7,28 @@ fn main() {
         y: i64,
         a: [i64; n]
     }
-    println!("{} {} {}", n, x, y);
-    println!("{:?}", a);
+    let m = 10000;
+    let mut dp1 = vec![false; 2 * m + 1];
+    let mut dp2 = vec![false; 2 * m + 1];
+    let mut dp3 = vec![0; 2 * m + 1];
+    dp1[a[0]] = true;
+    dp2[0] = true;
+    for i in 1..n {
+        let aa = a[i];
+        if i % 2 == 0 {
+            for j in -m..=(m - a) {
+                dp3[j + a] |= dp1[j];
+                dp3[j] |= dp1[j + a];
+            }
+        // ???
+        // swap(dp1, dp3);
+        } else {
+            for j in -m..=(m - a) {
+                dp3[j + a] |= dp2[j];
+                dp3[j] |= dp2[j + a];
+            }
+            // ???
+            // swap[dp2, dp3];
+        }
+    }
 }
