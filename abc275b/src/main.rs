223945c52414a8a03290a1870704d4555f816d67
Wataru NAKAI <watnk0806@gmail.com>
2022-10-30 22:41:42 +0900

    ABC275 B; AC!

---
+++ abc275b/src/main.rs
@@ -0,0 +1,27 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        a: i64,
+        b: i64,
+        c: i64,
+        d: i64,
+        e: i64,
+        f: i64,
+    }
+    let m = 998244353;
+    let mut abc = a % m;
+    abc *= b % m;
+    abc %= m;
+    abc *= c % m;
+    abc %= m;
+    let mut def = d % m;
+    def *= e % m;
+    def %= m;
+    def *= f % m;
+    def %= m;
+    let mut ans = abc - def;
+    ans += m;
+    ans %= m;
+    println!("{}", ans);
+}
