b89e11d56950107b962ee4fb134563b6c209ba81
Wataru NAKAI <watnk0806@gmail.com>
2022-11-04 23:12:07 +0900

    ABC229 D; AC...

---
+++ abc229d/src/main.rs
@@ -0,0 +1,26 @@
+use proconio::{input, marker::Chars};
+
+fn main() {
+    input! {
+        s: Chars,
+        k: usize,
+    }
+    let n = s.len();
+    let mut cnt = vec![0; n + 1];
+    for i in 0..n {
+        if s[i] == '.' {
+            cnt[i + 1] = cnt[i] + 1;
+        } else {
+            cnt[i + 1] = cnt[i];
+        }
+    }
+    let mut ans = 0;
+    let mut r = 0;
+    for l in 0..n {
+        while r <= n - 1 && cnt[r + 1] - cnt[l] <= k {
+            r = r + 1;
+        }
+        ans = ans.max(r - l);
+    }
+    println!("{}", ans);
+}
