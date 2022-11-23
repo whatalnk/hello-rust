9a4ede7f4ca67c7d02c5ea2a653bd1340cfabd56
Wataru NAKAI <watnk0806@gmail.com>
2022-11-23 23:52:28 +0900

    ABC278 C; Editorial ver.

--- abc278c/src/main.rs
+++ abc278c/src/main.rs
@@ -1,39 +1,21 @@
 use proconio::input;
-use std::collections::BTreeMap;
 use std::collections::BTreeSet;
 
 fn main() {
     input! {
-        n: usize,
+        _n: usize,
         q: usize,
         tab: [(i64, usize, usize); q],
     }
-    let mut m = BTreeMap::<usize, BTreeSet<usize>>::new();
+    let mut s = BTreeSet::<(usize, usize)>::new();
     for i in 0..q {
         let (t, a, b) = tab[i];
         if t == 1 {
-            let e = m.entry(a).or_insert(BTreeSet::<usize>::new());
-            e.insert(b);
+            s.insert((a, b));
         } else if t == 2 {
-            let e = m.entry(a).or_insert(BTreeSet::<usize>::new());
-            e.remove(&b);
+            s.remove(&(a, b));
         } else {
-            let mut ans = true;
-            if let Some(e) = m.get(&a) {
-                if !e.contains(&b) {
-                    ans = false;
-                }
-            } else {
-                ans = false;
-            }
-            if let Some(e) = m.get(&b) {
-                if !e.contains(&a) {
-                    ans = false;
-                }
-            } else {
-                ans = false;
-            }
-            if ans {
+            if s.contains(&(a, b)) && s.contains(&(b, a)) {
                 println!("Yes");
             } else {
                 println!("No");
