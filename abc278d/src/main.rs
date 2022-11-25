88443752ab918cae8edb60a33824365594afc294
Wataru NAKAI <watnk0806@gmail.com>
2022-11-25 12:08:54 +0900

    ABC278 D; TLE

--- abc278d/src/main.rs
+++ abc278d/src/main.rs
@@ -1,28 +1,24 @@
 use proconio::input;
-use proconio::marker::Chars;
 
 fn main() {
     input!(n: usize);
-    input!(a: [i64; n]);
+    input!(mut a: [i64; n]);
     input!(q: usize);
-    println!("{}", n);
-    println!("{:?}", a);
-    println!("{}", q);
     for _ in 0..q {
         input!(c: usize);
         match c {
             1 => {
                 input!(x: i64);
-                println!("{} {}", c, x);
+                a = vec![x; n];
             }
             2 => {
                 input!(i: usize);
                 input!(x: i64);
-                println!("{} {} {}", c, i, x);
+                a[i - 1] += x;
             }
             _ => {
                 input!(i: usize);
-                println!("{} {}", c, i);
+                println!("{}", a[i - 1]);
             }
         }
     }
