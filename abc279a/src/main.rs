600d0bfb12a881c7a0fa6e5eaa3eebe4009b82b9
Wataru NAKAI <watnk0806@gmail.com>
2022-11-26 22:52:07 +0900

    ABC279 A; AC!

---
+++ abc279a/src/main.rs
@@ -0,0 +1,16 @@
+use proconio::{input, marker::Chars};
+
+fn main() {
+    input! {
+        s: Chars
+    };
+    let mut ans = 0;
+    for c in s {
+        if c == 'v' {
+            ans += 1
+        } else {
+            ans += 2;
+        }
+    }
+    println!("{}", ans);
+}
