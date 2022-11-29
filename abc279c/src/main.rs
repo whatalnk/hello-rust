53ca0f8ffc802a841689edd19e5342e0e94cc5a3
Wataru NAKAI <watnk0806@gmail.com>
2022-11-29 21:32:56 +0900

    ABC279 C; AC!

--- abc279c/src/main.rs
+++ abc279c/src/main.rs
@@ -1,12 +1,33 @@
 use proconio::{input, marker::Chars};
 
+fn transpose(v: &Vec<Vec<char>>) -> Vec<String> {
+    let h = v.len();
+    let w = v[0].len();
+    let mut ret = vec![];
+    for i in 0..w {
+        let mut c = vec![];
+        for j in 0..h {
+            c.push(v[j][i]);
+        }
+        ret.push(c.iter().collect::<String>());
+    }
+    return ret;
+}
+
 fn main() {
     input! {
         h: usize,
-        w: usize,
+        _w: usize,
         s: [Chars; h],
         t: [Chars; h],
     }
-    println!("{:?}", s);
-    println!("{:?}", t);
+    let mut ss = transpose(&s);
+    ss.sort();
+    let mut tt = transpose(&t);
+    tt.sort();
+    if ss == tt {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
