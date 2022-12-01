112d94013f06d80a84bc42f3a99b8dff57650191
Wataru NAKAI <watnk0806@gmail.com>
2022-12-01 22:30:55 +0900

    ABC279 D; AC...

--- abc279d/src/main.rs
+++ abc279d/src/main.rs
@@ -1,12 +1,27 @@
 use proconio::input;
 
+fn f(a: f64, b: i64, n: i64) -> f64 {
+    return (b * n) as f64 + a / (n as f64 + 1.0).sqrt();
+}
 fn main() {
     input! {
         a: f64,
         b: i64
     }
-    for i in 0..=10 {
-        let g = i as f64 + 1.0;
-        println!("{}", (b * i) as f64 + a / g.sqrt());
+    let mut l = 0;
+    let mut r = a as i64 / b;
+    while r - l > 2 {
+        let m1 = (l * 2 + r) / 3;
+        let m2 = (l + r * 2) / 3;
+        if f(a, b, m1) > f(a, b, m2) {
+            l = m1;
+        } else {
+            r = m2;
+        }
     }
+    let mut ans = a;
+    for i in l..=r {
+        ans = ans.min(f(a, b, i));
+    }
+    println!("{}", ans);
 }
