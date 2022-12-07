2c7b26f54195d9c7f9276c80c9f34df84b2026b6
Wataru NAKAI <watnk0806@gmail.com>
2022-12-07 22:48:35 +0900

    ABC280 D; WA

--- abc280d/src/main.rs
+++ abc280d/src/main.rs
@@ -2,7 +2,27 @@ use proconio::input;
 
 fn main() {
     input! {
-        k: i64,
+        mut k: i64,
     }
-    println!("{}", k);
+    let mut v = vec![];
+    let mut p: i64 = 2;
+    while p * p <= k {
+        let mut e: i64 = 0;
+        while k % p == 0 {
+            k /= p;
+            e += 1;
+        }
+        if e > 0 {
+            v.push((p, e));
+        }
+        p += 1;
+    }
+    if k != 1 {
+        v.push((k, 1));
+    }
+    let mut ans: i64 = 0;
+    for (p, e) in &v {
+        ans = ans.max(p * e);
+    }
+    println!("{}", ans);
 }
