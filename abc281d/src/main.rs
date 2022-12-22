8f1048ad160740e81c651ee44108030f1428b8bd
Wataru NAKAI <watnk0806@gmail.com>
2022-12-22 22:27:59 +0900

    ABC281 D; AC...

--- abc281d/src/main.rs
+++ abc281d/src/main.rs
@@ -7,6 +7,22 @@ fn main() {
         d: i64,
         a: [i64; n],
     }
-    println!("{} {} {}", n, k, d);
-    println!("{:?}", a);
+    let mut dp = vec![vec![vec![-1; d as usize]; k + 1]; n + 1];
+    dp[0][0][0] = 0;
+    for i in 0..n {
+        for j in 0..k + 1 {
+            for kk in 0..(d as usize) {
+                if dp[i][j][kk] == -1 {
+                    continue;
+                }
+                dp[i + 1][j][kk] = dp[i + 1][j][kk].max(dp[i][j][kk]);
+                if j != k {
+                    dp[i + 1][j + 1][((kk as i64 + a[i]) % d) as usize] = dp[i + 1][j + 1]
+                        [((kk as i64 + a[i]) % d) as usize]
+                        .max(dp[i][j][kk] + a[i]);
+                }
+            }
+        }
+    }
+    println!("{}", dp[n][k][0]);
 }
