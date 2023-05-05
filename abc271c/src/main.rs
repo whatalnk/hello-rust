06b191e14f9be34c9cc276dbadc66d7e81b7b7f7
Wataru NAKAI <watnk0806@gmail.com>
2023-05-05 19:46:17 +0900

    cargo clippy

--- abc271c/src/main.rs
+++ abc271c/src/main.rs
@@ -8,9 +8,7 @@ fn main() {
     let mut vol = vec![false; n + 2];
     let mut sold: i64 = 0;
     for i in 0..n {
-        if a[i] >= vol.len() {
-            sold += 1;
-        } else if vol[a[i]] {
+        if a[i] >= vol.len() || vol[a[i]] {
             sold += 1;
         } else {
             vol[a[i]] = true;
