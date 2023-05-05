259fa3d238db24ba1e5e3e95e8b197de64d21086
Wataru NAKAI <watnk0806@gmail.com>
2023-05-05 20:48:01 +0900

    cargo clippy

--- abc198c/src/main.rs
+++ abc198c/src/main.rs
@@ -2,16 +2,17 @@ use proconio::input;
 
 fn main() {
     input! {
-        r: f64,
-        x: f64,
-        y: f64
+        r: i64,
+        x: i64,
+        y: i64
     }
-    let d = (x * x + y * y).sqrt();
-    if d == r {
+    let d2 = x * x + y * y;
+    let d = (d2 as f64).sqrt();
+    if d2 == r * r {
         println!("1");
-    } else if d <= r + r {
+    } else if d <= (r + r) as f64 {
         println!("2");
     } else {
-        println!("{}", (d / r).ceil());
+        println!("{}", (d / r as f64).ceil());
     }
 }
