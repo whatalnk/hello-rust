a2f6c45b2b8c7aa4d0bd549abf750931199ad7d3
Wataru NAKAI <watnk0806@gmail.com>
2023-05-09 11:07:30 +0900

    cargo clippy

--- abc197c/src/main.rs
+++ abc197c/src/main.rs
@@ -5,19 +5,18 @@ fn main() {
         n: usize,
         a: [i64; n]
     }
-    let mut res = 2000000000;
+    let mut res = 2_000_000_000;
     for i in 0..(1 << (n - 1)) {
         let mut xored = 0;
         let mut ored = 0;
-        for j in 0..=n {
-            if j < n {
-                ored |= a[j];
-            }
-            if j == n || (i >> j & 1) > 0 {
+        for (j, item) in a.iter().enumerate() {
+            ored |= item;
+            if (i >> j & 1) > 0 {
                 xored ^= ored;
                 ored = 0;
             }
         }
+        xored ^= ored;
         res = res.min(xored);
     }
     println!("{}", res);
