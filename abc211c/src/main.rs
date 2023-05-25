3d01fe21e2e73326f4d52b693b4a08cdeb517a51
Wataru NAKAI <watnk0806@gmail.com>
2023-05-25 10:31:34 +0900

    cargo clippy

--- abc211c/src/main.rs
+++ abc211c/src/main.rs
@@ -7,16 +7,16 @@ fn main() {
     }
     let n = s.len();
     s.insert(0, ' ');
-    let md = 1000000007;
+    let md = 1_000_000_007;
     let t: Vec<char> = " chokudai".chars().collect();
     let mut dp = vec![vec![0; 9]; n + 1];
     for i in 0..=n {
-        for j in 0..=8 {
+        for (j, tj) in t.iter().enumerate().take(8 + 1) {
             if j == 0 {
                 dp[i][j] = 1;
             } else if i == 0 {
                 dp[i][j] = 0;
-            } else if s[i] != t[j] {
+            } else if s[i] != *tj {
                 dp[i][j] = dp[i - 1][j];
             } else {
                 dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1];
