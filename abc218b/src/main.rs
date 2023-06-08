8b833a647d02a8068d9a486163afe58f0cf2c7b5
Wataru NAKAI <watnk0806@gmail.com>
2023-06-08 10:51:06 +0900

    cargo clippy

--- abc218b/src/main.rs
+++ abc218b/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     input! {
         p: [u8; 26]
     }
-    let a = 'a' as u8;
+    let a = b'a';
     let s: String = p.iter().map(|i| (a + i - 1) as char).collect();
     println!("{}", s);
 }
