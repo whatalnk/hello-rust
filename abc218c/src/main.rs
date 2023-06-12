066a6baebb0af750098f7d4e79d7bc68eff02f79
Wataru NAKAI <watnk0806@gmail.com>
2023-06-12 11:26:01 +0900

    cargo clippy

--- abc218c/src/main.rs
+++ abc218c/src/main.rs
@@ -1,7 +1,7 @@
 use proconio::input;
 use proconio::marker::Chars;
 
-fn rot(s: &Vec<Vec<char>>) -> Vec<Vec<char>> {
+fn rot(s: &[Vec<char>]) -> Vec<Vec<char>> {
     let mut ss = vec![];
     let n = s.len();
     for j in 0..n {
@@ -11,45 +11,43 @@ fn rot(s: &Vec<Vec<char>>) -> Vec<Vec<char>> {
         }
         ss.push(r);
     }
-    return ss;
+    ss
 }
 
-fn find_left_top(s: &Vec<Vec<char>>) -> (usize, usize) {
+fn find_left_top(s: &[Vec<char>]) -> (usize, usize) {
     let n = s.len();
     let mut ans = (0, 0);
-    for i in 0..n {
-        for j in 0..n {
-            if s[i][j] == '#' {
+    for (i, si) in s.iter().enumerate().take(n) {
+        for (j, sij) in si.iter().enumerate().take(n) {
+            if sij == &'#' {
                 ans = (i, j);
                 break;
             }
         }
     }
-    return ans;
+    ans
 }
 
-fn is_same(s: &Vec<Vec<char>>, t: &Vec<Vec<char>>) -> bool {
+fn is_same(s: &[Vec<char>], t: &[Vec<char>]) -> bool {
     let n = s.len();
-    let (si, sj) = find_left_top(&s);
-    let (ti, tj) = find_left_top(&t);
+    let (si, sj) = find_left_top(s);
+    let (ti, tj) = find_left_top(t);
     let offset_i = ti as i64 - si as i64;
     let offset_j = tj as i64 - sj as i64;
-    for i in 0..n {
-        for j in 0..n {
+    for (i, si) in s.iter().enumerate().take(n) {
+        for (j, sij) in si.iter().enumerate().take(n) {
             let ii = i as i64 + offset_i;
             let jj = j as i64 + offset_j;
             if ii >= 0 && ii < n as i64 && jj >= 0 && jj < n as i64 {
-                if s[i][j] != t[ii as usize][jj as usize] {
+                if sij != &t[ii as usize][jj as usize] {
                     return false;
                 }
-            } else {
-                if s[i][j] == '#' {
-                    return false;
-                }
+            } else if sij == &'#' {
+                return false;
             }
         }
     }
-    return true;
+    true
 }
 
 fn main() {
@@ -64,7 +62,7 @@ fn main() {
         println!("No");
         return;
     }
-    let mut s_ = s.clone();
+    let mut s_ = s;
     for _ in 0..4 {
         if is_same(&s_, &t) {
             println!("Yes");
@@ -73,5 +71,4 @@ fn main() {
         s_ = rot(&s_);
     }
     println!("No");
-    return;
 }
