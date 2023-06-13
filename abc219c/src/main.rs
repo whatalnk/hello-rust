f7b548cc7409d92c78d3f42bf958993266471bce
Wataru NAKAI <watnk0806@gmail.com>
2023-06-13 10:46:30 +0900

    cargo clippy

--- abc219c/src/main.rs
+++ abc219c/src/main.rs
@@ -10,7 +10,7 @@ fn main() {
     }
     let l = x.len();
     let mut y = Vec::new();
-    for i in ('a' as u8)..=('z' as u8) {
+    for i in b'a'..=b'z' {
         y.push(i as char);
     }
     let mut xy = HashMap::new();
@@ -20,16 +20,16 @@ fn main() {
         yx.insert(y[i], x[i]);
     }
     let mut sy = Vec::new();
-    for i in 0..n {
-        let ss: String = s[i]
+    for si in s.iter().take(n) {
+        let ss: String = si
             .iter()
             .map(|c| xy.get(&c).unwrap())
             .fold("".to_string(), |acc, c| acc + &c.to_string());
         sy.push(ss);
     }
     sy.sort();
-    for i in 0..n {
-        let ss: String = sy[i]
+    for syi in sy.iter().take(n) {
+        let ss: String = syi
             .as_str()
             .chars()
             .map(|c| yx.get(&c).unwrap())
