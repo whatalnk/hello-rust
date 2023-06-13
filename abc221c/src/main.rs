12470dd056d6ed8df983bab5a1977eb80d2bf473
Wataru NAKAI <watnk0806@gmail.com>
2023-06-13 10:53:28 +0900

    cargo clippy

--- abc221c/src/main.rs
+++ abc221c/src/main.rs
@@ -12,11 +12,11 @@ fn main() {
     for i in 0..(1 << l) {
         let mut left = 0;
         let mut right = 0;
-        for j in 0..l {
+        for (j, nj) in n.iter().enumerate().take(l) {
             if (i & (1 << j)) > 0 {
-                left = left * 10 + n[j].to_digit(10).unwrap();
+                left = left * 10 + nj.to_digit(10).unwrap();
             } else {
-                right = right * 10 + n[j].to_digit(10).unwrap();
+                right = right * 10 + nj.to_digit(10).unwrap();
             }
             ans = ans.max(left * right);
         }
