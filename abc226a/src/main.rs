bc69435d4eb80297257af478ba9f8e5d1eba8963
Wataru NAKAI <watnk0806@gmail.com>
2023-06-14 10:22:39 +0900

    cargo clippy

--- abc226a/src/main.rs
+++ abc226a/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     }
     let v: Vec<&str> = x.split_terminator('.').collect();
     let mut a = v[0].parse::<i32>().unwrap();
-    let b = v[1].chars().nth(0).unwrap().to_digit(10).unwrap();
+    let b = v[1].chars().next().unwrap().to_digit(10).unwrap();
     if b > 4 {
         a += 1;
     }
