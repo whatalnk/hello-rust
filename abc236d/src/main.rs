e11fde5684499bb904b5846ed2196c81aa11df49
Wataru NAKAI <watnk0806@gmail.com>
2023-06-18 14:27:36 +0900

    cargo clippy

--- abc236d/src/main.rs
+++ abc236d/src/main.rs
@@ -9,15 +9,15 @@ impl S {
     fn calc(&self, used: &mut Vec<bool>, v: &mut Vec<(usize, usize)>) -> i64 {
         if v.len() == self.n {
             let mut ret: i64 = 0;
-            for i in 0..self.n {
-                ret ^= self.a[v[i].0][v[i].1];
+            for vi in v.iter().take(self.n) {
+                ret ^= self.a[vi.0][vi.1];
             }
             return ret;
         }
 
         let mut l: usize = 0;
-        for i in 0..(2 * self.n) {
-            if !used[i] {
+        for (i, usedi) in used.iter().enumerate().take(2 * self.n) {
+            if !usedi {
                 l = i;
                 break;
             }
@@ -36,19 +36,17 @@ impl S {
         }
 
         used[l] = false;
-        return ret;
+        ret
     }
 }
 
 fn main() {
     input!(n: usize);
     let mut a = vec![vec![0; 2 * n]; 2 * n];
-    for i in 1..=(2 * n - 1) {
+    for i in 1..2 * n {
         input!(aa: [i64; 2 * n - i]);
-        for j in 0..(2 * n - i) {
-            a[i - 1][i + j] = aa[j];
-        }
+        a[i - 1][i..((2 * n - i) + i)].clone_from_slice(&aa[..(2 * n - i)]);
     }
-    let s = S { n: n, a: a.clone() };
+    let s = S { n, a };
     println!("{}", s.calc(&mut vec![false; 20], &mut vec![]));
 }
