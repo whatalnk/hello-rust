e59e0a3c686ee2880370cbfc4307be4446f231ba
Wataru NAKAI <watnk0806@gmail.com>
2023-06-19 10:46:33 +0900

    cargo clippy

--- abc239b/src/main.rs
+++ abc239b/src/main.rs
@@ -4,13 +4,9 @@ fn main() {
     input! {
         x: i64
     }
-    if x >= 0 {
+    if x >= 0 || x % 10 == 0 {
         println!("{}", x / 10);
     } else {
-        if x % 10 == 0 {
-            println!("{}", x / 10);
-        } else {
-            println!("{}", x / 10 - 1);
-        }
+        println!("{}", x / 10 - 1);
     }
 }
