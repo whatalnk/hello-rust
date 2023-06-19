2938e81eba8f27c390cdb09dfec01dcda6cacc62
Wataru NAKAI <watnk0806@gmail.com>
2023-06-19 10:31:51 +0900

    cargo clippy

--- abc237a/src/main.rs
+++ abc237a/src/main.rs
@@ -1,10 +1,10 @@
 use proconio::input;
 
-fn main(){
-    input!{
+fn main() {
+    input! {
         n: i64,
     }
-    if n >= -2147483648 &&  n < 2147483648 {
+    if n >= -2_147_483_648 && n < 2_147_483_648 {
         println!("Yes");
     } else {
         println!("No");
