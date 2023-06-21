75319c7b50e662434047c52c7d36c7e26b9d2284
Wataru NAKAI <watnk0806@gmail.com>
2023-06-21 10:28:56 +0900

    cargo clippy

--- abc244b/src/main.rs
+++ abc244b/src/main.rs
@@ -11,28 +11,26 @@ fn main() {
     let mut d = 'E';
     let mut dx = 1;
     let mut dy = 0;
-    for i in 0..n {
-        if s[i] == 'S' {
+    for si in s.iter().take(n) {
+        if si == &'S' {
             x += dx;
             y += dy;
+        } else if d == 'E' {
+            d = 'S';
+            dx = 0;
+            dy = -1;
+        } else if d == 'S' {
+            d = 'W';
+            dx = -1;
+            dy = 0;
+        } else if d == 'W' {
+            d = 'N';
+            dx = 0;
+            dy = 1;
         } else {
-            if d == 'E' {
-                d = 'S';
-                dx = 0;
-                dy = -1;
-            } else if d == 'S' {
-                d = 'W';
-                dx = -1;
-                dy = 0;
-            } else if d == 'W' {
-                d = 'N';
-                dx = 0;
-                dy = 1;
-            } else {
-                d = 'E';
-                dx = 1;
-                dy = 0;
-            }
+            d = 'E';
+            dx = 1;
+            dy = 0;
         }
     }
     println!("{} {}", x, y);
