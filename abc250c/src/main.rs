22a51fff5c64ccabe2c7784eb140bb944a0851fe
Wataru NAKAI <watnk0806@gmail.com>
2023-06-22 10:35:36 +0900

    cargo clippy

--- abc250c/src/main.rs
+++ abc250c/src/main.rs
@@ -17,10 +17,7 @@ fn main() {
     }
     for i in x {
         let l = hm.remove(&i).unwrap();
-        let mut r = l + 1;
-        if l == n {
-            r = l - 1;
-        }
+        let r = if l == n { l - 1 } else { l + 1 };
         b.swap(l - 1, r - 1);
         hm.insert(b[l - 1], l);
         hm.insert(b[r - 1], r);
