a182c774e495204c5203516bab189b40453e67fb
Wataru NAKAI <watnk0806@gmail.com>
2023-06-22 10:24:00 +0900

    cargo clippy

--- abc247b/src/main.rs
+++ abc247b/src/main.rs
@@ -9,11 +9,11 @@ fn main() {
         let (s1, t1) = &st[i];
         let mut ans1 = true;
         let mut ans2 = true;
-        for j in 0..n {
+        for (j, stj) in st.iter().enumerate().take(n) {
             if i == j {
                 continue;
             }
-            let (s2, t2) = &st[j];
+            let (s2, t2) = &stj;
             if s1 == s2 || s1 == t2 {
                 ans1 = false;
             }
