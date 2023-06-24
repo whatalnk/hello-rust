4dfbfba4a5bc348fca13990d2ef410b088407a7b
Wataru NAKAI <watnk0806@gmail.com>
2023-06-24 22:52:45 +0900

    cargo clippy

--- abc256d/src/main.rs
+++ abc256d/src/main.rs
@@ -5,12 +5,12 @@ fn main() {
         n: usize,
         lr: [(usize, usize); n]
     }
-    let mmax = 200000 + 1;
+    let mmax = 200_000 + 1;
     let mut m = vec![0; mmax];
-    for i in 0..n {
-        let (l, r) = lr[i];
-        m[l] += 1;
-        m[r] -= 1;
+    for lri in lr.iter().take(n) {
+        let (l, r) = lri;
+        m[*l] += 1;
+        m[*r] -= 1;
     }
     for i in 1..mmax {
         m[i] += m[i - 1];
@@ -18,11 +18,11 @@ fn main() {
     let mut ans = vec![];
     let mut on = false;
     let mut left = 0;
-    for i in 0..mmax {
-        if on && m[i] == 0 {
+    for (i, mi) in m.iter().enumerate().take(mmax) {
+        if on && mi == &0 {
             on = false;
             ans.push((left, i));
-        } else if !on && m[i] > 0 {
+        } else if !on && mi > &0 {
             on = true;
             left = i;
         }
