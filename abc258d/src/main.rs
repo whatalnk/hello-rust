a6d25ed7848fcaf41d1b4080f8516f45eb1bd57e
Wataru NAKAI <watnk0806@gmail.com>
2023-06-25 12:01:28 +0900

    cargo clippy

--- abc258d/src/main.rs
+++ abc258d/src/main.rs
@@ -9,9 +9,9 @@ fn main() {
     let mut aa = ab[0].0 + ab[0].1;
     let mut bmin = ab[0].1;
     let mut ans = aa + ((x - 1) as i64) * bmin;
-    for i in 1..(n.min(x)) {
-        aa += ab[i].0 + ab[i].1;
-        bmin = bmin.min(ab[i].1);
+    for (i, abi) in ab.iter().enumerate().take(n.min(x)).skip(1) {
+        aa += abi.0 + abi.1;
+        bmin = bmin.min(abi.1);
         let ans_ = aa + ((x - i - 1).max(0) as i64) * bmin;
         ans = ans.min(ans_);
     }
