bf159f92fede02af8ab384c3ffdb647c8f1f7352
Wataru NAKAI <watnk0806@gmail.com>
2023-06-25 13:09:01 +0900

    cargo clippy

--- abc260d/src/main.rs
+++ abc260d/src/main.rs
@@ -12,10 +12,10 @@ fn main() {
     let mut ans = vec![-1; n + 1];
     let mut hm = HashMap::<usize, Vec<usize>>::new();
     let mut bts = BTreeSet::<usize>::new();
-    for i in 0..n {
-        let c = p[i];
+    for (i, pi) in p.iter().enumerate().take(n) {
+        let c = *pi;
         let mut flag = false;
-        for &elem in bts.range((Included(&c), Included(&n))) {
+        if let Some(&elem) = bts.range((Included(&c), Included(&n))).next() {
             hm.entry(elem).and_modify(|e| e.push(c));
             flag = true;
             bts.remove(&elem);
@@ -29,20 +29,19 @@ fn main() {
                     bts.insert(c);
                 }
             }
-            break;
         }
         if !flag {
             if k == 1 {
                 ans[c] = (i + 1) as isize;
             } else {
-                let e = hm.entry(c).or_insert(Vec::new());
+                let e = hm.entry(c).or_insert_with(Vec::new);
                 e.push(c);
                 bts.insert(c);
             }
         }
     }
 
-    for i in 1..=n {
-        println!("{}", ans[i]);
+    for ansi in ans.iter().take(n + 1).skip(1) {
+        println!("{}", ansi);
     }
 }
