58c3fc033981cdfeabf9c3caa06257ce6086fd5a
Wataru NAKAI <watnk0806@gmail.com>
2023-06-25 11:44:09 +0900

    cargo clippy

--- abc257a/src/main.rs
+++ abc257a/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
         n: usize,
         x: usize
     }
-    let c: u8 = 'A' as u8;
+    let c: u8 = b'A';
     let ans = (c - 1) + (x / n) as u8;
     if x % n == 0 {
         println!("{}", ans as char);
