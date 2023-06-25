3afb79f6e762b3bf1262cd7ca6c16a286e9545f2
Wataru NAKAI <watnk0806@gmail.com>
2023-06-25 11:57:30 +0900

    cargo clippy

--- abc258b/src/main.rs
+++ abc258b/src/main.rs
@@ -19,21 +19,21 @@ fn main() {
     let mut ans = vec![];
     for i in 0..(n as isize) {
         for j in 0..(n as isize) {
-            for k in 0..8 {
+            for dk in &d {
                 let mut cnt = 0;
                 let mut s = vec![];
                 let mut cx = i;
                 let mut cy = j;
                 while cnt < n {
-                    let mut nx = cx + d[k].0;
+                    let mut nx = cx + dk.0;
                     if nx < 0 {
-                        nx = (n as isize) + nx;
+                        nx += n as isize;
                     } else if nx >= (n as isize) {
                         nx -= n as isize;
                     }
-                    let mut ny = cy + d[k].1;
+                    let mut ny = cy + dk.1;
                     if ny < 0 {
-                        ny = (n as isize) + ny;
+                        ny += n as isize;
                     } else if ny >= (n as isize) {
                         ny -= n as isize;
                     }
