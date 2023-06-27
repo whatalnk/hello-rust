d328f8627d5f5f57d3ef96ef775ff9e8d691bb21
Wataru NAKAI <watnk0806@gmail.com>
2023-06-27 11:28:40 +0900

    cargo clippy

--- abc261c/src/main.rs
+++ abc261c/src/main.rs
@@ -7,13 +7,13 @@ fn main() {
         s: [String; n]
     }
     let mut hm = HashMap::new();
-    for i in 0..n {
-        if hm.contains_key(&s[i]) {
-            println!("{}({})", s[i], hm.get(&s[i]).unwrap());
-            hm.entry(&s[i]).and_modify(|e| *e += 1);
+    for si in s.iter().take(n) {
+        if hm.contains_key(si) {
+            println!("{}({})", si, hm.get(&si).unwrap());
+            hm.entry(si).and_modify(|e| *e += 1);
         } else {
-            println!("{}", s[i]);
-            hm.insert(&s[i], 1);
+            println!("{}", si);
+            hm.insert(si, 1);
         }
     }
 }
