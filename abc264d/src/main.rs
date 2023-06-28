0d8ae0d5d5dcf8723e1602f839b2cd4bbd66eddd
Wataru NAKAI <watnk0806@gmail.com>
2023-06-28 11:49:49 +0900

    cargo clippy

--- abc264d/src/main.rs
+++ abc264d/src/main.rs
@@ -1,14 +1,14 @@
 use proconio::input;
 use proconio::marker::Chars;
 
-fn find_index(c: char, s: &Vec<char>) -> usize {
+fn find_index(c: char, s: &[char]) -> usize {
     let mut ans = 0;
-    for i in 0..s.len() {
-        if c == s[i] {
+    for (i, si) in s.iter().enumerate() {
+        if &c == si {
             ans = i;
         }
     }
-    return ans;
+    ans
 }
 
 fn main() {
@@ -17,8 +17,8 @@ fn main() {
     }
     let t = "atcoder".chars().collect::<Vec<char>>();
     let mut ans = 0;
-    for i in 0..s.len() {
-        let mut si = find_index(t[i], &s);
+    for (i, ti) in t.iter().enumerate().take(s.len()) {
+        let mut si = find_index(*ti, &s);
         let ti = i;
         while si != ti {
             s.swap(si, si - 1);
