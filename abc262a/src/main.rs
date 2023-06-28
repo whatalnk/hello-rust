be5b6c33872c47e2bd8bc2163ef269fd7de2d913
Wataru NAKAI <watnk0806@gmail.com>
2023-06-28 11:30:49 +0900

    cargo clippy

--- abc262a/src/main.rs
+++ abc262a/src/main.rs
@@ -1,15 +1,20 @@
 use proconio::input;
+use std::cmp::Ordering;
 
 fn main() {
     input! {
         y: i64,
     }
     let r = y % 4;
-    if r < 2 {
-        println!("{}", y + 2 - r);
-    } else if r == 2 {
-        println!("{}", y);
-    } else {
-        println!("{}", y + 3);
+    match r.cmp(&2) {
+        Ordering::Less => {
+            println!("{}", y + 2 - r);
+        }
+        Ordering::Equal => {
+            println!("{}", y);
+        }
+        Ordering::Greater => {
+            println!("{}", y + 3);
+        }
     }
 }
