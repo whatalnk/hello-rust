abf3be49518b9baaed704d4af19a031bca967140
Wataru NAKAI <watnk0806@gmail.com>
2023-06-28 12:10:27 +0900

    cargo clippy

--- abc268d/src/main.rs
+++ abc268d/src/main.rs
@@ -1,17 +1,13 @@
 use itertools::Itertools;
 use proconio::input;
-use std::process::exit;
+use std::{process::exit, string::ToString};
 
-fn contain_ans(ans: &String, t: &Vec<String>) -> bool {
-    let x = t.binary_search(ans);
-    if x.is_ok() {
-        return true;
-    } else {
-        return false;
-    }
+fn contain_ans(ans: &str, t: &[String]) -> bool {
+    let x = t.binary_search(&ans.to_string());
+    x.is_ok()
 }
 
-fn dfs(cur: usize, s: &Vec<String>, t: &Vec<String>, remain: i64, ans: String) {
+fn dfs(cur: usize, s: &[String], t: &[String], remain: i64, ans: String) {
     if remain < 0 {
         return;
     }
@@ -22,16 +18,16 @@ fn dfs(cur: usize, s: &Vec<String>, t: &Vec<String>, remain: i64, ans: String) {
         }
         return;
     }
-    if ans.len() > 0 && (!ans.ends_with("_")) {
-        let mut ans_ = ans.clone();
+    if !ans.is_empty() && (!ans.ends_with('_')) {
+        let mut ans_ = ans;
         ans_.push('_');
         dfs(cur, s, t, remain, ans_);
     } else {
         let mut ans_ = ans.clone();
         ans_.push_str(&s[cur]);
         dfs(cur + 1, s, t, remain, ans_);
-        if ans.len() > 0 {
-            let mut ans_ = ans.clone();
+        if !ans.is_empty() {
+            let mut ans_ = ans;
             ans_.push('_');
             dfs(cur, s, t, remain - 1, ans_);
         }
@@ -47,10 +43,10 @@ fn main() {
     }
     tt.sort();
     let mut remain = 16;
-    for i in 0..n {
-        remain = remain - (ss[i].len() as i64);
+    for ssi in ss.iter().take(n) {
+        remain -= ssi.len() as i64;
     }
-    remain = remain - ((n - 1) as i64);
+    remain -= (n - 1) as i64;
     ss.into_iter()
         .permutations(n)
         .for_each(|perm| dfs(0, &perm, &tt, remain, "".to_string()));
