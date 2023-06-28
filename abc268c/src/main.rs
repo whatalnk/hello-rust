1de662b8f3318a9f489d0fe36f84501f7cf70415
Wataru NAKAI <watnk0806@gmail.com>
2023-06-28 12:04:45 +0900

    cargo clippy

--- abc268c/src/main.rs
+++ abc268c/src/main.rs
@@ -6,15 +6,15 @@ fn main() {
         p: [i64; n],
     }
     let mut cnt = vec![0; n];
-    for i in 0..n {
+    for (i, pi) in p.iter().enumerate().take(n) {
         for j in 0..3 {
-            let ii = (p[i] - 1 - (i as i64) + (j as i64) + (n as i64)) % (n as i64);
+            let ii = (pi - 1 - (i as i64) + (j as i64) + (n as i64)) % (n as i64);
             cnt[ii as usize] += 1;
         }
     }
     let mut ans: i64 = 0;
-    for i in 0..n {
-        ans = ans.max(cnt[i]);
+    for cnti in cnt.iter().take(n) {
+        ans = ans.max(*cnti);
     }
     println!("{}", ans);
 }
