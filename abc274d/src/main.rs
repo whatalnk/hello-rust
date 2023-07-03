b4f09a6dc18751e25485ae66aa1fa35cc9faa464
Wataru NAKAI <watnk0806@gmail.com>
2023-07-03 11:10:41 +0900

    cargo clippy

--- abc274d/src/main.rs
+++ abc274d/src/main.rs
@@ -12,9 +12,9 @@ fn main() {
     let mut dp2 = vec![false; 2 * (m as usize) + 1];
     dp1[(m + a[0]) as usize] = true;
     dp2[m as usize] = true;
-    for i in 1..n {
+    for (i, ai) in a.iter().enumerate().take(n).skip(1) {
         let mut dp3 = vec![false; 2 * (m as usize) + 1];
-        let aa = a[i];
+        let aa = ai;
         if i % 2 == 0 {
             for j in -m..=(m - aa) {
                 dp3[(m + j + aa) as usize] |= dp1[(m + j) as usize];
