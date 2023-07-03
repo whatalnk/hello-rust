9cf22c2f81ccfc47291f015643bc0c4b18fce937
Wataru NAKAI <watnk0806@gmail.com>
2023-07-03 10:59:17 +0900

    cargo clippy

--- abc272d/src/main.rs
+++ abc272d/src/main.rs
@@ -18,42 +18,40 @@ fn main() {
     d[1][1] = 0;
     let mut q = VecDeque::new();
     q.push_back((1, 1));
-    loop {
-        if let Some(p) = q.pop_front() {
-            for i in 0..moveto.len() {
-                for (s1, s2) in vec![(1, 1), (1, -1), (-1, 1), (-1, -1)] {
-                    let ni = (p.0 as isize) + (moveto[i].0 as isize) * s1;
-                    let nj = (p.1 as isize) + (moveto[i].1 as isize) * s2;
-                    if ni > 0
-                        && ni <= n as isize
-                        && nj > 0
-                        && nj <= n as isize
-                        && d[ni as usize][nj as usize] < 0
-                    {
-                        d[ni as usize][nj as usize] = d[p.0][p.1] + 1;
-                        q.push_back((ni as usize, nj as usize));
-                    }
-                    let ni = (p.0 as isize) + (moveto[i].1 as isize) * s1;
-                    let nj = (p.1 as isize) + (moveto[i].0 as isize) * s2;
-                    if ni > 0
-                        && ni <= n as isize
-                        && nj > 0
-                        && nj <= n as isize
-                        && d[ni as usize][nj as usize] < 0
-                    {
-                        d[ni as usize][nj as usize] = d[p.0][p.1] + 1;
-                        q.push_back((ni as usize, nj as usize));
-                    }
+
+    while let Some(p) = q.pop_front() {
+        for movetoi in &moveto {
+            for (s1, s2) in &[(1, 1), (1, -1), (-1, 1), (-1, -1)] {
+                let ni = (p.0 as isize) + (movetoi.0 as isize) * s1;
+                let nj = (p.1 as isize) + (movetoi.1 as isize) * s2;
+                if ni > 0
+                    && ni <= n as isize
+                    && nj > 0
+                    && nj <= n as isize
+                    && d[ni as usize][nj as usize] < 0
+                {
+                    d[ni as usize][nj as usize] = d[p.0][p.1] + 1;
+                    q.push_back((ni as usize, nj as usize));
+                }
+                let ni = (p.0 as isize) + (movetoi.1 as isize) * s1;
+                let nj = (p.1 as isize) + (movetoi.0 as isize) * s2;
+                if ni > 0
+                    && ni <= n as isize
+                    && nj > 0
+                    && nj <= n as isize
+                    && d[ni as usize][nj as usize] < 0
+                {
+                    d[ni as usize][nj as usize] = d[p.0][p.1] + 1;
+                    q.push_back((ni as usize, nj as usize));
                 }
             }
-        } else {
-            break;
         }
     }
-    for i in 1..=n {
+
+    for di in d.iter().take(n + 1).skip(1) {
         println!(
             "{}",
-            &d[i][1..]
+            &di[1..]
                 .iter()
                 .map(|x| x.to_string())
                 .collect::<Vec<String>>()
