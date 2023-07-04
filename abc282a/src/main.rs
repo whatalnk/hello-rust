c5306b2d8152c902f0c44db52e850f70836065e3
Wataru NAKAI <watnk0806@gmail.com>
2023-07-04 10:49:44 +0900

    cargo clippy

--- abc282a/src/main.rs
+++ abc282a/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     input! {
         k: u8,
     };
-    let a = 'A' as u8;
+    let a = b'A';
     let mut ans = "".to_string();
     for i in 0..k {
         ans += &((a + i) as char).to_string();
