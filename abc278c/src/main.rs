cb888367636209096c00b78f40894ed2481ac4be
Wataru NAKAI <watnk0806@gmail.com>
2023-07-04 10:45:02 +0900

    cargo clippy

--- abc278c/src/main.rs
+++ abc278c/src/main.rs
@@ -8,18 +8,16 @@ fn main() {
         tab: [(i64, usize, usize); q],
     }
     let mut s = BTreeSet::<(usize, usize)>::new();
-    for i in 0..q {
-        let (t, a, b) = tab[i];
-        if t == 1 {
-            s.insert((a, b));
-        } else if t == 2 {
-            s.remove(&(a, b));
+    for tabi in tab.iter().take(q) {
+        let (t, a, b) = tabi;
+        if t == &1 {
+            s.insert((*a, *b));
+        } else if t == &2 {
+            s.remove(&(*a, *b));
+        } else if s.contains(&(*a, *b)) && s.contains(&(*b, *a)) {
+            println!("Yes");
         } else {
-            if s.contains(&(a, b)) && s.contains(&(b, a)) {
-                println!("Yes");
-            } else {
-                println!("No");
-            }
+            println!("No");
         }
     }
 }
