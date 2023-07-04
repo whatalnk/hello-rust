3d3cf6a423d645dbd3b3a1006a0afa3fb4d2610a
Wataru NAKAI <watnk0806@gmail.com>
2023-07-04 10:15:14 +0900

    cargo clippy

--- abc277a/src/main.rs
+++ abc277a/src/main.rs
@@ -6,8 +6,8 @@ fn main() {
         x: i64,
         p: [i64; n],
     }
-    for i in 0..n {
-        if p[i] == x {
+    for (i, pi) in p.iter().enumerate().take(n) {
+        if pi == &x {
             println!("{}", i + 1);
             return;
         }
