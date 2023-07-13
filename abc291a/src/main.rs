b8960a14eb4baa27f7c5f95066a523531ef25713
Wataru NAKAI <watnk0806@gmail.com>
2023-07-13 21:52:09 +0900

    ABC291 A; AC!

---
+++ abc291a/src/main.rs
@@ -0,0 +1,14 @@
+use std::println;
+
+use proconio::{input, marker::Chars};
+
+fn main() {
+    input! {
+        s: Chars
+    }
+    for (i, c) in s.iter().enumerate() {
+        if c.is_ascii_uppercase() {
+            println!("{}", i + 1);
+        }
+    }
+}
