483f82aac3c4d5aa6eef041821d88b9769524954
Wataru NAKAI <watnk0806@gmail.com>
2023-10-20 11:13:20 +0900

    ABC324 A; AC!

---
+++ abc324a/src/main.rs
@@ -0,0 +1,17 @@
+use std::println;
+
+use proconio::input;
+
+fn main() {
+    input! {
+        n: usize,
+        a: [i64; n],
+    }
+    for i in 0..(n - 1) {
+        if a[i] != a[i + 1] {
+            println!("No");
+            return;
+        }
+    }
+    println!("Yes");
+}
